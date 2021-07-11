53bef97060613bacbb51e9d8fbb124b90e90274c
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-07-11 20:00:07 +0900

    Add: groupsテーブル用ファイル

--- db/migrate/01_create_groups.rb
+++ db/migrate/01_create_groups.rb
@@ -1,7 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[6.1]
   def change
     create_table :groups do |t|
-
+      t.string :name, null: false, limit: 31
+      t.datetime :deleted_at, null: false
       t.timestamps
     end
   end
