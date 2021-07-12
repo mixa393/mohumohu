ff71482ab3c68918568f84fb5f244f72c34b34da
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-07-12 10:18:28 +0900

    Add: usersテーブルにカラム追加

--- db/migrate/02_create_users.rb
+++ db/migrate/02_create_users.rb
@@ -1,9 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :name
-
-      t.timestamps
+      t.string :name, limit: 31, null: false
+      t.string :email, limit: 127, null: false
+      t.string :password
+      t.string :image_path
+      t.datetime :deleted_at
+      t.timestampsrbenv
     end
   end
 end
