8a548a491df39092fb2a565cc27e46b74f0973af
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-07-15 17:02:42 +0900

    Fix: 外部キーのnullを不許可

--- db/migrate/02_create_users.rb
+++ db/migrate/02_create_users.rb
@@ -1,15 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.bigint :team_id
+      t.bigint :team_id, null: false
       t.string :name, limit: 31, null: false
       t.string :email, limit: 127, null: false
-      t.string :password,null: false
+      t.string :password, null: false
       t.datetime :deleted_at
       t.timestamps
     end
 
-    add_foreign_key :users,:teams
+    add_foreign_key :users, :teams
 
   end
 end
