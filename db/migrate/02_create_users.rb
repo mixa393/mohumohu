d765c77fb8abed570f7b899c720b311970dbc634
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-07-15 13:29:34 +0900

    Fix: 外部キー制約の書き方を修正

--- db/migrate/02_create_users.rb
+++ db/migrate/02_create_users.rb
@@ -1,13 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
+      t.bigint :group_id
       t.string :name, limit: 31, null: false
       t.string :email, limit: 127, null: false
       t.string :password,null: false
       t.datetime :deleted_at
       t.timestamps
-
-      add_foreign_key :users,:groups
     end
+
+    add_foreign_key :users,:groups
+
   end
 end
