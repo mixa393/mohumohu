c8e81596505adfa6a7eb2e5578f47c26c1c04dd1
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-07-15 14:14:03 +0900

    Fix: groupsをteamsに変更

--- db/migrate/01_create_teams.rb
+++ db/migrate/01_create_teams.rb
@@ -1,6 +1,6 @@
-class CreateGroups < ActiveRecord::Migration[6.1]
+class CreateTeams < ActiveRecord::Migration[6.1]
   def change
-    create_table :groups do |t|
+    create_table :teams do |t|
       t.string :name, null: false, limit: 31
       t.datetime :deleted_at, null: false
       t.timestamps
