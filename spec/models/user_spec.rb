dc837ab2b075f4c031770bf7eb6dc10b058ad0c8
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-07-24 19:34:22 +0900

    Fix: Userのit文整理

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe User do
+    it "is valid with a first name, last name, email, and password"
+    it "名前が無ければ無効"
+    it "名前が32文字以上なら無効"
+    it "メールアドレスが無ければ無効"
+    it "メールアドレスが127文字以上なら無効"
+    it "メールアドレスがフォーマットに即していなければ無効"
+    it "パスワードが無ければ無効"
+    it "パスワードが255文字以上であれば無効"
+    it "team_idが無ければ無効"
+    it "重複したメールアドレスなら無効"
+  end
 end
