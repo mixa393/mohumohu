eba98813deebfb3285a13fddf228165a1acf70f7
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-07-27 22:31:10 +0900

    Fix: Teamsスペックファイル

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Team, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "名前があれば有効" do
+    team = Team.new(
+      name: "testteam"
+    )
+    expect(team).to be_valid
+  end
+
+  it "名前が無ければ無効" do
+    team = Team.new(
+      name: nil
+    )
+    expect(team).not_to be_valid
+  end
+
+  it "名前が32文字以上なら無効" do
+    team = Team.new(
+      name: "a"*32
+    )
+    expect(team).not_to be_valid
+  end
 end
