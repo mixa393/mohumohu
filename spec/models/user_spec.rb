897d91e858782d9d5303ca531dc22535a91061eb
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-07-28 21:28:20 +0900

    Add: userスペック

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,103 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @team = Team.create(
+      name: "testteam"
+    )
+  end
 
   describe User do
-    it "is valid with a first name, last name, email, and password"
-    it "名前が無ければ無効"
-    it "名前が32文字以上なら無効"
-    it "メールアドレスが無ければ無効"
-    it "メールアドレスが127文字以上なら無効"
-    it "メールアドレスがフォーマットに即していなければ無効"
-    it "パスワードが無ければ無効"
-    it "パスワードが255文字以上であれば無効"
-    it "team_idが無ければ無効"
-    it "重複したメールアドレスなら無効"
+    it "名前、メールアドレス、パスワード、team_idがあれば有効" do
+      user = User.new(
+        name: "test",
+        email: "email@test.com",
+        password: "testpass",
+        team_id: @team.id
+      )
+      expect(user).to be_valid
+    end
+
+    it "名前が無ければ無効" do
+      user = User.new(
+        name: nil,
+        email: "email@test.com",
+        password: "testpass",
+        team_id: @team.id
+      )
+      expect(user).not_to be_valid
+    end
+
+    it "名前が32文字以上なら無効" do
+      user = User.new(
+        name: "a" * 32,
+        email: "email@test.com",
+        password: "testpass",
+        team_id: @team.id
+      )
+      expect(user).not_to be_valid
+    end
+
+    it "メールアドレスが無ければ無効" do
+      user = User.new(
+        name: "test",
+        email: nil,
+        password: "testpass",
+        team_id: @team.id
+      )
+      expect(user).not_to be_valid
+    end
+
+    it "メールアドレスが127文字以上なら無効" do
+      user = User.new(
+        name: "test",
+        email: "a" * 127,
+        password: "testpass",
+        team_id: @team.id
+      )
+      expect(user).not_to be_valid
+    end
+
+    it "メールアドレスがフォーマットに即していなければ無効" do
+
+    end
+
+    it "パスワードが無ければ無効" do
+      user = User.new(
+        name: "test",
+        email: "email@test.com",
+        password: nil,
+        team_id: @team.id
+      )
+      expect(user).not_to be_valid
+    end
+
+    it "パスワードが256文字以上であれば無効" do
+      user = User.new(
+        name: "test",
+        email: "email@test.com",
+        password: "a" * 256,
+        team_id: @team.id
+      )
+      expect(user).not_to be_valid
+    end
+
+    it "重複したメールアドレスなら無効" do
+      User.create(
+        name: "first_user",
+        email: "email@test.com",
+        password: "first_user_pass",
+        team_id: @team.id
+      )
+
+      second_user = User.new(
+        name: "second_user",
+        email: "email@test.com",
+        password: "second_user_pass",
+        team_id: @team.id
+      )
+      expect(second_user).not_to be_valid
+    end
   end
 end
