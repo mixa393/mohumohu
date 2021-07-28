7ba2a04f5e7f0cdb08c4171b918e52f38e876509
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-07-28 21:48:48 +0900

    Add: laundry_historyスペック

--- spec/models/laundry_history_spec.rb
+++ spec/models/laundry_history_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe LaundryHistory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @team = Team.create(
+      name: "test_team"
+    )
+
+    @user = User.create(
+      name: "test_user",
+      email: "email@test.com",
+      password: "test_pass",
+      team_id: @team.id
+    )
+
+    @laundry = Laundry.create(
+      name: "laundry_name",
+      days: 5,
+      team_id: @team.id,
+      user_id: @user.id
+    )
+  end
+
+  it "user_id,laundry_idがあれば有効" do
+    laundry_history = LaundryHistory.new(
+      user_id: @user.id,
+      laundry_id: @laundry.id
+    )
+    expect(laundry_history).to be_valid
+  end
 end
