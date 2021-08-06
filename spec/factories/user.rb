58d7ff99ed44b6c83e49159ba00c2ff2aede5bb5
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-06 21:33:07 +0900

    Fix: Fakerによりuserのデータを1つに

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,11 +5,4 @@ FactoryBot.define do
     password { Faker::Internet.password }
     team_id { 1 }
   end
-
-  factory :second_user, class: User do
-    name { Faker::Internet.username }
-    email { Faker::Internet.unique.email }
-    password { Faker::Internet.password }
-    team_id { 1 }
-  end
 end
