e9782ab5c897e19ef6efad585906c4b8a99f9864
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-09 18:41:25 +0900

    Fix: ディレクトリ分化

--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  root to: 'users#index'
-  get 'users', to: 'users#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
