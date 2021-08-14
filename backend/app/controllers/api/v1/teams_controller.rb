57fe9ca538f5b60d0f200bd97d933497ecc0972e
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-14 21:28:11 +0900

    Add: create途中まで

--- backend/app/controllers/api/v1/teams_controller.rb
+++ backend/app/controllers/api/v1/teams_controller.rb
@@ -1,27 +1,22 @@
 class Api::V1::TeamsController < ApplicationController
   before_action :set_team, only: [:show, :update, :destroy]
-
-  # def index
-  #   render json: { status: 200, teams: Team.all }
-  # end
+  protect_from_forgery
+  skip_before_action :verify_authenticity_token
 
   def show
-    debugger
     render json: { status: 200, team: @team }
   end
 
-  #
-  # def create
-  #   team = Team.new(team_params)
-  #
-  #   if team.save
-  #     render json: { status: 200, team: team }
-  #   else
-  #     render json: { status: 500, message: "Teamの作成に失敗しました" }
-  #   end
-  # end
-  #
-  #
+  def create
+    team = Team.new(team_params)
+
+    if team.save
+      render json: { status: 200, team: team }
+    else
+      render json: { status: 500, message: "Teamの作成に失敗しました" }
+    end
+  end
+
   # def update
   #   if @team.update(team_params)
   #     render json: { status: 200, message: 'Updated the post', data: @team }
