a127f460579b7478ad74ec8d47d45b8a9369b272
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-17 16:42:39 +0900

    Add: createメソッドのテスト

--- backend/spec/requests/teams_spec.rb
+++ backend/spec/requests/teams_spec.rb
@@ -4,13 +4,23 @@ RSpec.describe "TeamsAPI", type: :request do
 
   let(:team) { FactoryBot.create(:team) }
   let(:request_header) { { "X-Requested-With" => "XMLHttpRequest" } }
+  let(:valid_param) { Faker::Team.name }
 
-  it "POST /api/v1/teams" do
+  # teams#create
+  context "POST /api/v1/teams" do
+    it 'status:200を返す' do
+      post '/api/v1/teams', headers: request_header, params: { name: valid_param }
+      expect(response.status).to eq(200)
+    end
+
+    it '新しいデータが作成される' do
+      expect { post '/api/v1/teams', headers: request_header, params: { name: valid_param } }
+        .to change(Team, :count).by(+1)
+    end
   end
 
+  # teams#show
   context "GET /api/v1/teams/:id" do
-    # teams#show
-
     it "status:200を返すこと" do
       get "/api/v1/teams/#{team.id}", headers: request_header
       expect(response.status).to eq(200)
