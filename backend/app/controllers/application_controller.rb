81773f816deff367574aa0896fc9d4e633e29de5
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-17 15:38:18 +0900

    Add: CSRF対策

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :check_xhr_header
   skip_forgery_protection
+
+
+  private
+
+  def check_xhr_header
+    return if request.xhr?
+
+    render json: { error: 'forbidden' }, status: :forbidden
+  end
 end
