c6abd4f95ab217845d41e1723125f84fd0d43d8f
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-17 15:33:47 +0900

    Add: showのルーティング入れ忘れ

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :teams, only: [:create, :update, :destroy]
+      resources :teams, only: [:show, :create, :update, :destroy]
       post 'teams/:id', to: 'teams#remove'
       # resources :users
     end
