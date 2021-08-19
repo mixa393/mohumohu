bfa42957e32cce0cbe76b7a77b08f8302b78b91e
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-19 16:30:36 +0900

    Fix: usersのCRUDメソッド

--- frontend/src/lib/api/users.js
+++ frontend/src/lib/api/users.js
@@ -1,16 +1,21 @@
 import client from "./client"
 
-// user一覧を取得
-export const getUsers = () => {
-    return client.get("/users")
+// user情報取得
+export const getUsers = (id) => {
+    return client.get(`/users/${id}`)
 }
 
-// userを新規作成
+// user作成
 export const createUser = (data) => {
     return client.post("/users", data)
 }
 
-// userを削除
+// user情報更新
+export const updateUsers = (data) => {
+    return client.put("/users", data)
+}
+
+// user削除
 export const deleteUser = (id) => {
     return client.delete(`/users/${id}`)
 }
