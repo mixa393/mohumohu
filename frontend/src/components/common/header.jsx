1d9ecc813fba16433984ec27d6d3d81fa1b1bac0
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-20 12:41:03 +0900

    Add: header mock

--- frontend/src/components/common/header.jsx
+++ frontend/src/components/common/header.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import setting from "../../images/common/settings.png";
 
-const Header = () => {
+const Header = ({date}) => {
     return (
-            <header>header</header>
+        <header className="bg-pink-400 h-20">
+            <div className="flex h-full justify-between items-center">
+                <p className="mx-9 text-5xl text-white font-bold">{date}</p>
+                <nav className="mx-9 h-2/5">
+                    <img src={setting} alt="ユーザー設定編集" className="h-full max-h-full w-auto"/>
+                </nav>
+            </div>
+        </header>
     );
 }
 
