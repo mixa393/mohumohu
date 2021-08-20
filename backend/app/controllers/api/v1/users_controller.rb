453d06a28a76099d5f14f1fd454d4ffb9ce13aa9
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-20 13:11:14 +0900

    Fix: teams移し

---
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,45 @@
+class API::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  def show
+    render json: { status: 200, data: @user }
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: { status: 200, data: user }
+    else
+      render json: { status: 400, message: "Userの作成に失敗しました", data: user.errors }
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: { status: 200, data: @user }
+    else
+      render json: { status: 500, data: @user.errors }
+    end
+  end
+
+  # 論理削除
+  def destroy
+    if @user.update(deleted_at: Time.now)
+      render json: { status: 200, data: @user }
+    else
+      render json: { status: 500, message: "Userの削除に失敗しました" }
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.permit(:name)
+  end
+
+end
