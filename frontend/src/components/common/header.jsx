50cedff8bb74e44b587eb7d7e42a8291ca2d19c8
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-23 11:43:43 +0900

    Fix: header調整

--- frontend/src/components/common/header.jsx
+++ frontend/src/components/common/header.jsx
@@ -3,9 +3,9 @@ import setting from "../../images/common/settings.png";
 
 const Header = ({date}) => {
     return (
-        <header className="bg-pink-400 h-20">
+        <header className="bg-pink-400 h-16">
             <div className="flex h-full justify-between items-center">
-                <p className="mx-9 text-5xl text-white font-bold">{date}</p>
+                <p className="mx-9 text-3xl text-white font-bold">{date}.</p>
                 <nav className="mx-9 h-2/5">
                     <img src={setting} alt="ユーザー設定編集" className="h-full max-h-full w-auto"/>
                 </nav>
