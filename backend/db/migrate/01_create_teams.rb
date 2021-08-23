fceb12b24d9bcb9d069aa63847c62504c42bc7fc
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-23 19:24:13 +0900

    Add: DB location_idカラム

--- backend/db/migrate/01_create_teams.rb
+++ backend/db/migrate/01_create_teams.rb
@@ -2,6 +2,7 @@ class CreateTeams < ActiveRecord::Migration[6.1]
   def change
     create_table :teams do |t|
       t.string :name, null: false, limit: 31
+      t.string :location_id, null: false, limit: 6
       t.datetime :deleted_at
       t.timestamps
     end
