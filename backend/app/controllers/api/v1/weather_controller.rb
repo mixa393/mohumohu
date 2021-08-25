10d3da433c489bb0ed6e485beedf81ed21f00a88
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-25 19:14:43 +0900

    Fix: メモ追加

--- backend/app/controllers/api/v1/weather_controller.rb
+++ backend/app/controllers/api/v1/weather_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::WeatherController < ApplicationController
   def get
     location_id = weather_params[:location_id]
 
+    # 天気取得API https://weather.tsukumijima.net/
     uri = URI.parse("https://weather.tsukumijima.net/api/forecast/city/#{location_id}")
 
     # NET::HTTPを利用してAPIを叩く
@@ -11,6 +12,7 @@ class Api::V1::WeatherController < ApplicationController
 
     data = {
       city: res["location"]["city"],
+      # [0]は今日のデータ
       telop: res["forecasts"][0]["telop"],
       chance_of_rain_am: res["forecasts"][0]["chanceOfRain"]["T06_12"],
       chance_of_rain_pm: res["forecasts"][0]["chanceOfRain"]["T12_18"],
--- backend/spec/requests/weather_spec.rb
+++ backend/spec/requests/weather_spec.rb
@@ -4,13 +4,14 @@ require 'net/http'
 RSpec.describe "Weathers", type: :request do
 
   let(:request_header) { { "X-Requested-With" => "XMLHttpRequest" } }
-  let(:valid_params) { { location_id: 130010 } }
+  let(:valid_param) { { location_id: 130010 } }
 
   it "GET /weather" do
-    get "/api/v1/weather", headers: request_header, params: valid_params
+    get "/api/v1/weather", headers: request_header, params: valid_param
 
     json = JSON.parse(response.body)
 
+    # 例として東京のデータ(location_id:130010)を取得
     expect(json["data"]["city"]).to eq("東京")
     expect(response.status).to eq(200)
   end
