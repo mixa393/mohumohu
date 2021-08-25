1d5fac4e85f963d945c1000263412aac6e11280b
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-25 16:12:15 +0900

    Fix: factorybotに必須ではないカラム追加

--- backend/spec/factories/laundry.rb
+++ backend/spec/factories/laundry.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     team
     user
     name { Faker::String.random(length: 3..12) }
-    days { 5 }
+    days { rand(5..10) }
+    description { "#{Faker::String.random(length: 3..12)}の説明文" }
+    notice { description { "#{Faker::String.random(length: 3..12)}の通知文" } }
   end
 end
