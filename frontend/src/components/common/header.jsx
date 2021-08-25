bf71c75135954e955210bf6b9bae667f1a1df87d
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-25 14:54:38 +0900

    Fix: header画像中央寄せ

--- frontend/src/components/common/header.jsx
+++ frontend/src/components/common/header.jsx
@@ -6,14 +6,14 @@ import setting from "../../images/common/setting.svg"
 const Header = ({date}) => {
     return (
         <header className="h-16 grid grid-cols-10">
-            <div className="h-16 col-span-6 bg-pink-400">
-                <img src={sun} alt="ダッシュボード" className="h-3/5 w-auto mx-auto mt-3"/>
+            <div className="h-16 col-span-6 bg-pink-400 flex justify-items-center items-center">
+                <img src={sun} alt="ダッシュボード" className="h-3/5 w-auto mx-auto"/>
             </div>
-            <div className="h-16 col-span-3 bg-pink-300">
-                <img src={menu} alt="洗濯物リスト" className="h-3/5 w-auto m-auto mt-3"/>
+            <div className="h-16 col-span-3 bg-pink-300 flex justify-items-center items-center">
+                <img src={menu} alt="洗濯物リスト" className="h-3/5 w-auto mx-auto"/>
             </div>
-            <nav className="h-16 col-span-1 bg-pink-200">
-                <img src={setting} alt="ユーザー設定編集" className="h-3/5 w-auto m-auto mt-3"/>
+            <nav className="h-16 col-span-1 bg-pink-200 flex justify-items-center items-center">
+                <img src={setting} alt="ユーザー設定編集" className="h-3/5 w-auto mx-auto"/>
             </nav>
         </header>
     );
