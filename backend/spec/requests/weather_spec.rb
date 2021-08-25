5e4d0caa9b35ce27cd83d45434b2c90c757d9c84
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-25 19:10:41 +0900

    Add: リクエストテスト

--- backend/spec/requests/weather_spec.rb
+++ backend/spec/requests/weather_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
+require 'net/http'
 
 RSpec.describe "Weathers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+
+  let(:request_header) { { "X-Requested-With" => "XMLHttpRequest" } }
+  let(:valid_params) { { location_id: 130010 } }
+
+  it "GET /weather" do
+    get "/api/v1/weather", headers: request_header, params: valid_params
+
+    json = JSON.parse(response.body)
+
+    expect(json["data"]["city"]).to eq("東京")
+    expect(response.status).to eq(200)
   end
 end
