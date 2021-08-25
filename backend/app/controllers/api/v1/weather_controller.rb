bcdd641c6ac4fe376d026b0a6f171a10559558a2
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-25 19:10:22 +0900

    Add: APIからのデータ取得

--- backend/app/controllers/api/v1/weather_controller.rb
+++ backend/app/controllers/api/v1/weather_controller.rb
@@ -1,2 +1,30 @@
 class Api::V1::WeatherController < ApplicationController
+
+  def get
+    location_id = weather_params[:location_id]
+
+    uri = URI.parse("https://weather.tsukumijima.net/api/forecast/city/#{location_id}")
+
+    # NET::HTTPを利用してAPIを叩く
+    responce = Net::HTTP.get(uri)
+    res = JSON.parse(responce)
+
+    data = {
+      city: res["location"]["city"],
+      telop: res["forecasts"][0]["telop"],
+      chance_of_rain_am: res["forecasts"][0]["chanceOfRain"]["T06_12"],
+      chance_of_rain_pm: res["forecasts"][0]["chanceOfRain"]["T12_18"],
+      image_url: res["forecasts"][0]["image"]["url"]
+    }
+
+    render json: {
+      status: 200,
+      data: data
+    }
+  end
+
+  def weather_params
+    params.permit(:location_id)
+  end
+
 end
