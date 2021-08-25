cabd9c90ec2a6f6fdacacde8fd1e6394433ac6b0
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-25 14:53:38 +0900

    Add: routerの記述

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,37 @@
+import {
+    Router,
+    Switch,
+    Route,
+} from "react-router";
 import './css/App.css';
+import Header from './components/common/header'
+import Footer from './components/common/footer'
 import UsersIndex from "./pages/users/index";
+import LaundriesIndex from "./pages/laundries";
 
 function App() {
     return (
-        <div className="App">
-            <UsersIndex/>
-        </div>
+        <Router>
+            <Header/>
+            <div className="App">
+                <Switch>
+                    <Route path={"/"}>
+                        <UsersIndex/>
+                    </Route>
+                    <Route path={"/weekly"}>
+                        <LaundriesIndex/>
+                    </Route>
+                    <Route path={"/register"}>
+                        <UsersIndex/>
+                    </Route>
+                    <Route path={"/settings"}>
+                        <UsersIndex/>
+                    </Route>
+                </Switch>
+            </div>
+            <Footer/>
+        </Router>
+
     );
 }
 
