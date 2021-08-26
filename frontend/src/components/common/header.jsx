41c66d368f56f3c17173a763afc376b82b406743
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-26 20:56:23 +0900

    Fix: menu表示

--- frontend/src/components/common/header.jsx
+++ frontend/src/components/common/header.jsx
@@ -3,59 +3,76 @@ import {Link, useLocation} from "react-router-dom"
 import sun from "../../images/common/sun.svg";
 import tshirt from "../../images/common/tshirts.svg"
 import menu from "../../images/common/menu.svg";
-import Menu from "./menu"
+import MenuButton from "../common/menuButton"
+import Menu from "../common/menu"
+import "../../css/menu.css"
 
 const Header = ({date}) => {
-    const location = useLocation()
 
+    //headerの幅を変える
     const [width, setWidth] = useState({
         index: "",
         laundries: "",
         setting: ""
     })
 
+    const location = useLocation()
+
     useEffect(() => {
-        const { pathname } = location
+        const {pathname} = location
         if (pathname === "/") {
             setWidth({
-                index: "col-span-6",
+                index: "col-span-7",
                 laundries: "col-span-3",
                 setting: "col-span-1"
             })
         } else if (pathname === "/laundries") {
             setWidth({
                 index: "col-span-3",
-                laundries: "col-span-6",
+                laundries: "col-span-7",
                 setting: "col-span-1"
             })
         } else {
             setWidth({
-                index: "col-span-2",
-                laundries: "col-span-2",
-                setting: "col-span-6"
+                index: "col-span-5",
+                laundries: "col-span-5",
+                setting: "col-span-1"
             })
         }
     }, [location])
 
+    const [visible, setVisible] = useState(false)
+
+    const toggleMenu = (e) => {
+        setVisible(!visible);
+
+        e.stopPropagation();
+    }
+
+
     return (
-        <header className="h-16 grid grid-cols-10">
-            <div className={width.index}>
-                <Link className="h-16 bg-pink-400 flex justify-items-center items-center" to={"/"}>
-                    <img src={sun} alt="ダッシュボード" className="h-3/5 w-auto mx-auto"/>
-                </Link>
-            </div>
-            <div className={width.laundries}>
-                <Link className="h-16 bg-pink-300 flex justify-items-center items-center" to="/laundries">
-                    <img src={tshirt} alt="洗濯物リスト" className="h-3/5 w-auto mx-auto"/>
-                </Link>
-            </div>
-            <nav className={width.setting}>
-                {/*<Link className="h-16 bg-pink-100 flex justify-items-center items-center" to={"/setting"}>*/}
-                {/*    <img src={menu} alt="設定編集" className="h-2/5 w-auto mx-auto"/>*/}
-                {/*</Link>*/}
-                <Menu right width={'20%'}/>
-            </nav>
-        </header>
+        <>
+            <header>
+                <div className="h-16 grid grid-cols-11">
+                    <div className={width.index}>
+                        <Link className="h-16 bg-pink-400 flex justify-items-center items-center" to={"/"}>
+                            <img src={sun} alt="ダッシュボード" className="h-3/5 w-auto mx-auto"/>
+                        </Link>
+                    </div>
+                    <div className={width.laundries}>
+                        <Link className="h-16 bg-pink-300 flex justify-items-center items-center" to="/laundries">
+                            <img src={tshirt} alt="洗濯物リスト" className="h-3/5 w-auto mx-auto"/>
+                        </Link>
+                    </div>
+                    <nav className={width.setting}>
+                        <MenuButton handleMouseDown={toggleMenu}/>
+                        <Menu handleMouseDown={toggleMenu}
+                              menuVisibility={visible}/>
+                    </nav>
+                </div>
+            </header>
+
+        </>
     );
 }
 
