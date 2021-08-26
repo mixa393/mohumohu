ee0ca1beae4718c268add93c2fef0cf6f03a3ca7
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-26 14:49:36 +0900

    Fix: リンク先によってheaderの幅を変更

--- frontend/src/components/common/header.jsx
+++ frontend/src/components/common/header.jsx
@@ -1,7 +1,8 @@
-import React from "react";
-import menu from "../../images/common/menu.svg";
+import React, {useEffect, useState} from "react";
+import {Link, useLocation} from "react-router-dom"
 import sun from "../../images/common/sun.svg";
-import setting from "../../images/common/setting.svg"
+import tshirt from "../../images/common/tshirts.svg"
+import menu from "../../images/common/menu.svg";
 
 const Header = ({date}) => {
     return (
@@ -9,11 +10,15 @@ const Header = ({date}) => {
             <div className="h-16 col-span-6 bg-pink-400 flex justify-items-center items-center">
                 <img src={sun} alt="ダッシュボード" className="h-3/5 w-auto mx-auto"/>
             </div>
-            <div className="h-16 col-span-3 bg-pink-300 flex justify-items-center items-center">
-                <img src={menu} alt="洗濯物リスト" className="h-3/5 w-auto mx-auto"/>
+            <div className={width.laundries}>
+                <Link className="h-16 bg-pink-300 flex justify-items-center items-center" to="/laundries">
+                    <img src={tshirt} alt="洗濯物リスト" className="h-3/5 w-auto mx-auto"/>
+                </Link>
             </div>
-            <nav className="h-16 col-span-1 bg-pink-200 flex justify-items-center items-center">
-                <img src={setting} alt="ユーザー設定編集" className="h-3/5 w-auto mx-auto"/>
+            <nav className={width.setting}>
+                <Link className="h-16 bg-pink-100 flex justify-items-center items-center" to={"/setting"}>
+                    <img src={menu} alt="設定編集" className="h-2/5 w-auto mx-auto"/>
+                </Link>
             </nav>
         </header>
     );
