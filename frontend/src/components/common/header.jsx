5a6f00d3957f542e99b3ad78d43a8087987e2800
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-26 16:55:03 +0900

    Add: headerにrouter関係追加

--- frontend/src/components/common/header.jsx
+++ frontend/src/components/common/header.jsx
@@ -5,10 +5,43 @@ import tshirt from "../../images/common/tshirts.svg"
 import menu from "../../images/common/menu.svg";
 
 const Header = ({date}) => {
+    const location = useLocation()
+
+    const [width, setWidth] = useState({
+        index: "",
+        laundries: "",
+        setting: ""
+    })
+
+    useEffect(() => {
+        const { pathname } = location
+        if (pathname === "/") {
+            setWidth({
+                index: "col-span-6",
+                laundries: "col-span-3",
+                setting: "col-span-1"
+            })
+        } else if (pathname === "/laundries") {
+            setWidth({
+                index: "col-span-3",
+                laundries: "col-span-6",
+                setting: "col-span-1"
+            })
+        } else {
+            setWidth({
+                index: "col-span-2",
+                laundries: "col-span-2",
+                setting: "col-span-6"
+            })
+        }
+    }, [location])
+
     return (
         <header className="h-16 grid grid-cols-10">
-            <div className="h-16 col-span-6 bg-pink-400 flex justify-items-center items-center">
-                <img src={sun} alt="ダッシュボード" className="h-3/5 w-auto mx-auto"/>
+            <div className={width.index}>
+                <Link className="h-16 bg-pink-400 flex justify-items-center items-center" to={"/"}>
+                    <img src={sun} alt="ダッシュボード" className="h-3/5 w-auto mx-auto"/>
+                </Link>
             </div>
             <div className={width.laundries}>
                 <Link className="h-16 bg-pink-300 flex justify-items-center items-center" to="/laundries">
