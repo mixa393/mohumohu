d7b24fd97307bc79e21352462006f6810c1d8f74
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-28 15:29:50 +0900

    Fix: laundries用のlistをコンポーネント化

--- frontend/src/components/laundries/list.jsx
+++ frontend/src/components/laundries/list.jsx
@@ -1,10 +1,10 @@
-import React,{useEffect,useState} from "react"
+import React, {useEffect, useState} from "react"
 import dayjs from "dayjs";
+import ListItem from "./listItem";
 
 const List = () => {
-    const today = dayjs()
     const [date, setDate] = useState({
-        threeDaysLater:"",
+        threeDaysLater: "",
         fourDaysLater: "",
         fiveDaysLater: "",
         sixDaysLater: "",
@@ -12,6 +12,7 @@ const List = () => {
     })
 
     useEffect(() => {
+        const today = dayjs()
         setDate({
             threeDaysLater: today.add(3, "d").format('MM/DD'),
             fourDaysLater: today.add(4, "d").format('MM/DD'),
@@ -19,83 +20,47 @@ const List = () => {
             sixDaysLater: today.add(6, "d").format('MM/DD'),
             sevenDaysLater: today.add(7, "d").format('MM/DD')
         })
-    }, [today])
+
+    }, [])
+
+    const [tableHeader, setTableHeader] = useState("")
+
 
     return (
         <>
             <div className="relative">
-                <table className="table-fixed laundries-table fixed">
-                    <thead>
-                    <tr className="h-32">
-                        <th className="w-32 border-2 border-dotted border-gray-100">アイテム</th>
-                    </tr>
-                    <tr className="h-32">
-                        <th className="w-32 border-2 border-dotted border-gray-100">アイテム</th>
-                    </tr>
-                    <tr className="h-32">
-                        <th className="w-32 border-2 border-dotted border-gray-100">アイテム</th>
-                    </tr>
-                    <tr className="h-32">
-                        <th className="w-32 border-2 border-dotted border-gray-100">アイテム</th>
-                    </tr>
-                    </thead>
-                </table>
-                <table className="table-fixed laundries-table overflow-x-scroll whitespace-nowrap">
-                    <thead>
-                    <tr className="h-32">
-                        <th className="w-32 border-2 border-dotted border-gray-100">アイテム</th>
-                        <th className="w-24 border-2 border-dotted border-gray-100">今日</th>
-                        <th className="w-24 border-2 border-dotted border-gray-100">明日</th>
-                        <th className="w-24 border-2 border-dotted border-gray-100">{date.threeDaysLater}</th>
-                        <th className="w-24 border-2 border-dotted border-gray-100">{date.fourDaysLater}</th>
-                        <th className="w-24 border-2 border-dotted border-gray-100">{date.fiveDaysLater}</th>
-                        <th className="w-24 border-2 border-dotted border-gray-100">{date.sixDaysLater}</th>
-                        <th className="w-24 border-2 border-dotted border-gray-100">{date.sevenDaysLater}</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr className="bg-pink-100 h-32">
-                        <td className="border-2 border-dotted border-gray-100">アイテム名</td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                    </tr>
-                    <tr className="h-32">
-                        <td>アイテム名</td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                    </tr>
-                    <tr className="bg-pink-100">
-                        <td>アイテム名</td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                    </tr>
-                    <tr>
-                        <td>新規追加</td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                        <td className="border-2 border-dotted border-gray-100"></td>
-                    </tr>
-                    </tbody>
-                </table>
+                <div className="overflow-auto w-full">
+                    <table className="table-fixed laundries-table whitespace-nowrap min-w-full"
+                           style={{width: '70rem'}}>
+                        <thead>
+                        <tr className="h-32">
+                            <th className="border-2 border-dotted border-gray-100">アイテム</th>
+                            <th className="border-2 border-dotted border-gray-100">今日</th>
+                            <th className="border-2 border-dotted border-gray-100">明日</th>
+                            <th className="border-2 border-dotted border-gray-100">{date.threeDaysLater}</th>
+                            <th className="border-2 border-dotted border-gray-100">{date.fourDaysLater}</th>
+                            <th className="border-2 border-dotted border-gray-100">{date.fiveDaysLater}</th>
+                            <th className="border-2 border-dotted border-gray-100">{date.sixDaysLater}</th>
+                            <th className="border-2 border-dotted border-gray-100">{date.sevenDaysLater}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        <ListItem/>
+                        <ListItem/>
+                        <tr>
+                            <td>新規追加</td>
+                            <td className="border-2 border-dotted border-gray-100"></td>
+                            <td className="border-2 border-dotted border-gray-100"></td>
+                            <td className="border-2 border-dotted border-gray-100"></td>
+                            <td className="border-2 border-dotted border-gray-100"></td>
+                            <td className="border-2 border-dotted border-gray-100"></td>
+                            <td className="border-2 border-dotted border-gray-100"></td>
+                            <td className="border-2 border-dotted border-gray-100"></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </>
     )
