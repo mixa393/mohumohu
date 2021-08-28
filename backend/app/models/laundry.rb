82482d02f027318a3a9076a72c8206dbf301137f
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-28 14:57:03 +0900

    Fix: daysとwash_atのバリデーション変更

--- backend/app/models/laundry.rb
+++ backend/app/models/laundry.rb
@@ -5,6 +5,9 @@ class Laundry < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 127 }
   validates :description, length: { maximum: 255 }
-  validates :days, presence: true, unless: :wash_at?, numericality: { only_integer: true }
-  validates :wash_at, presence: true, unless: :days?
+
+  validates :days, presence: true, unless: :wash_at?
+  validates :days, numericality: { only_integer: true }, allow_nil: true
+
+  validates :wash_at, presence: true
 end
