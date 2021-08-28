c966364f1f21800419ae128b42eba3339dabe2d6
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-28 15:29:00 +0900

    Fix: headerとfooterをapp.jsに出す

--- frontend/src/pages/laundries/index.jsx
+++ frontend/src/pages/laundries/index.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import "../../css/laundries.css";
-import Header from "../../components/common/header";
 import List from "../../components/laundries/list";
-import Footer from "../../components/common/footer";
 
 
 const LaundriesIndex = () => {
+
+
     return (
         <>
-            <Header/>
             <List/>
-            <Footer/>
         </>
     );
 }
