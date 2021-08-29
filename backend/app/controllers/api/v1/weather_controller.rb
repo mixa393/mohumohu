dbc5b2e6f8c6eba16d34f8203144c58067d70d91
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-29 19:04:19 +0900

    Fix: ドキュメントっぽく書いたはいいものの違う気がする

--- backend/app/controllers/api/v1/weather_controller.rb
+++ backend/app/controllers/api/v1/weather_controller.rb
@@ -3,9 +3,15 @@ class Api::V1::WeatherController < ApplicationController
   # require 'net/http'
 
   def get
+    # 天気取得API https://weather.tsukumijima.net を利用して天気データを返却する
+    # @param [VARCHAR(6)] location_id 地区ID
+    # @return [json] status,data
+    # data = {city: 都市名, telop: 天気, chance_of_rain_am: 午前の降水確率,
+    #         chance_of_rain_pm: 午後の降水確率, image_url:天気画像のURL}
+
     location_id = params[:location_id]
 
-    # 天気取得API https://weather.tsukumijima.net/
+
     uri = URI.parse("https://weather.tsukumijima.net/api/forecast/city/#{location_id}")
 
     # NET::HTTPを利用してAPIを叩く
@@ -13,19 +19,11 @@ class Api::V1::WeatherController < ApplicationController
     res = JSON.parse(responce)
 
     data = {
-      # 都市名
+
       city: res["location"]["city"],
-
-      # ["forecasts"]に配列で3日間のデータが入っている
-      # [0]は今日のデータ
-
-      # 今日の大まかな天気
       telop: res["forecasts"][0]["telop"],
-      # 午前の降水確率
       chance_of_rain_am: res["forecasts"][0]["chanceOfRain"]["T06_12"],
-      # 午後の降水確率
       chance_of_rain_pm: res["forecasts"][0]["chanceOfRain"]["T12_18"],
-      # 天気の画像
       image_url: res["forecasts"][0]["image"]["url"]
     }
 
