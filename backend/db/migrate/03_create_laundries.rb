a8aa29a1e32b5b9882b1983db53de9ed02a1746f
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-30 15:24:06 +0900

    Fix: wash_atカラムのデフォルト追加

--- backend/db/migrate/03_create_laundries.rb
+++ backend/db/migrate/03_create_laundries.rb
@@ -5,8 +5,8 @@ class CreateLaundries < ActiveRecord::Migration[6.1]
       t.bigint :user_id, null: false
       t.string :name, null: false, limit: 127
       t.string :description, limit: 255
-      t.integer :days, default: 7, comment: "次の洗濯までの期間、デフォルトは7日"
-      t.date :wash_at, null: false, comment: "次回の洗濯日"
+      t.integer :days, comment: "次の洗濯までの期間"
+      t.date :wash_at, null: false, default: Time.now.to_date, comment: "次回の洗濯日"
       t.text :notice, comment: "洗濯期間が過ぎたときの通知文"
       t.string :image_path, default: "default.svg", null: false, limit: 127
       t.datetime :deleted_at
