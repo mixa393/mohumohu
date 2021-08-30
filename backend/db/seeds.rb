806a668fd5fc155cd7b67927f56c91c95f083b90
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-30 11:27:47 +0900

    Fix: laundriesのseedデータ

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,7 +7,7 @@ end
 
 # user作成
 Team.all.each do |team|
-  password= Faker::Internet.password
+  password = Faker::Internet.password
 
   team.users.create!(
     name: Faker::Internet.username,
@@ -17,14 +17,19 @@ Team.all.each do |team|
   )
 end
 
-# # laundry作成
-# User.all.each do |n|
-#   user.laundries.create!(
-#     team_id: Team.find(n),
-#     name: Faker::String.random(length: 3..12)
-#   )
-# end
-#
+# laundry作成
+User.all.each do |user|
+  3.times do
+    user.laundries.create!(
+      team_id: user.team_id,
+      name: Faker::String.random(length: 3..12),
+      wash_at: Time.now.to_date + rand(3..7),
+      description: "#{Faker::String.random(length: 3..12)}の説明文",
+      notice:"#{Faker::String.random(length: 3..12)}の洗濯期限になりました。"
+    )
+  end
+end
+
 # # laundry_history作成
 # Laundry.all.each do |n|
 #   laundry.laundry_histories.create!(
