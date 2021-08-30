b6d46611812865777d3b2faf5b08919427514dc7
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-30 16:43:46 +0900

    Fix: カラム名をimage、デフォルトをdefaultに変更

--- backend/db/migrate/03_create_laundries.rb
+++ backend/db/migrate/03_create_laundries.rb
@@ -8,7 +8,7 @@ class CreateLaundries < ActiveRecord::Migration[6.1]
       t.integer :days, comment: "次の洗濯までの期間"
       t.date :wash_at, null: false, default: Time.now.to_date + 7, comment: "次回の洗濯日"
       t.text :notice, comment: "洗濯期間が過ぎたときの通知文"
-      t.string :image_path, default: "default.svg", null: false, limit: 127
+      t.string :image, default: "default", null: false, limit: 127
       t.datetime :deleted_at
       t.timestamps
     end
