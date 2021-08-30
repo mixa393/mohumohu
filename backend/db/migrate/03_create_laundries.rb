3f0b885b9b0ee014e00d180ac53ded1e56faa699
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-30 15:24:30 +0900

    Fix: wash_atカラムのデフォルトを7日後に修正

--- backend/db/migrate/03_create_laundries.rb
+++ backend/db/migrate/03_create_laundries.rb
@@ -6,7 +6,7 @@ class CreateLaundries < ActiveRecord::Migration[6.1]
       t.string :name, null: false, limit: 127
       t.string :description, limit: 255
       t.integer :days, comment: "次の洗濯までの期間"
-      t.date :wash_at, null: false, default: Time.now.to_date, comment: "次回の洗濯日"
+      t.date :wash_at, null: false, default: Time.now.to_date + 7, comment: "次回の洗濯日"
       t.text :notice, comment: "洗濯期間が過ぎたときの通知文"
       t.string :image_path, default: "default.svg", null: false, limit: 127
       t.datetime :deleted_at
