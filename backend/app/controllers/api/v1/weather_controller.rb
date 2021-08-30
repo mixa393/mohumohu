e8d0c9c8772e5c485f7f18a2cc91ee081942e059
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-30 11:13:34 +0900

    Add: 例外処理

--- backend/app/controllers/api/v1/weather_controller.rb
+++ backend/app/controllers/api/v1/weather_controller.rb
@@ -16,25 +16,24 @@ class Api::V1::WeatherController < ApplicationController
 
     uri = URI.parse("https://weather.tsukumijima.net/api/forecast/city/#{location_id}")
 
-    # NET::HTTPを利用してAPIを叩く
-    responce = Net::HTTP.get(uri)
-    res = JSON.parse(responce)
+    begin
+      # NET::HTTPを利用してAPIを叩く
+      responce = Net::HTTP.get(uri)
+      res = JSON.parse(responce)
 
-    data = {
+      data = {
+        city: res["location"]["city"],
+        telop: res["forecasts"][0]["telop"],
+        chance_of_rain_am: res["forecasts"][0]["chanceOfRain"]["T06_12"],
+        chance_of_rain_pm: res["forecasts"][0]["chanceOfRain"]["T12_18"],
+        image_url: res["forecasts"][0]["image"]["url"]
+      }
 
-      city: res["location"]["city"],
-      telop: res["forecasts"][0]["telop"],
-      chance_of_rain_am: res["forecasts"][0]["chanceOfRain"]["T06_12"],
-      chance_of_rain_pm: res["forecasts"][0]["chanceOfRain"]["T12_18"],
-      image_url: res["forecasts"][0]["image"]["url"]
-    }
-
-    if res
       render json: {
         status: 200,
         data: data
       }
-    else
+    rescue
       render json: {
         status: 400,
         message: "天気の取得に失敗しました"
