c0d7b5beec2d07c8e50ecdf8ae834a3e877644d1
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-30 13:31:55 +0900

    Add: weekly request test

--- backend/spec/requests/weekly_spec.rb
+++ backend/spec/requests/weekly_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Weeklies", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "Weekly", type: :request do
+  let(:team) { FactoryBot.create(:team) }
+  let(:laundries) { FactoryBot.create_list(:laundry, 5, team_id: team.id) }
+  let(:request_header) { { "X-Requested-With" => "XMLHttpRequest" } }
+
+  it "GET /weekly/:id" do
+    team_id = team.id
+    print team_id
+    print laundries
+    get "/api/v1/weekly/#{team_id}", headers: request_header
+
+    json = JSON.parse(response.body)
+
+    expect(json["data"].first["name"]).to eq(laundries.first.name)
+    expect(response.status).to eq(200)
   end
 end
