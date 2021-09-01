bca33c0d7a5f7b58599f18be2013664a99499bc2
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-01 21:01:56 +0900

    Fix: モデルにdevise情報

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-            # Include default devise modules.
-            devise :database_authenticatable, :registerable,
-                    :recoverable, :rememberable, :trackable, :validatable,
-                    :confirmable, :omniauthable
-            include DeviseTokenAuth::Concerns::User
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  # :confirmable, :omniauthable
+  include DeviseTokenAuth::Concerns::User
+
   belongs_to :team
   has_many :laundry_histories
   has_many :laundries
@@ -11,8 +12,8 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 31 }
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6, maximum: 255 },on: :create
-  validates :password_confirmation, presence: true, length: { minimum: 6, maximum: 255 },on: :create
+  validates :password, presence: true, length: { minimum: 6, maximum: 255 }, on: :create
+  validates :password_confirmation, presence: true, length: { minimum: 6, maximum: 255 }, on: :create
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }, length: { maximum: 127 }
