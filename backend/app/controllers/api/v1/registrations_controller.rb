4a313bee171a748123785bf6a2efe281955be499
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-02 16:55:54 +0900

    Add: 参考サイトコピペ

--- backend/app/controllers/api/v1/registrations_controller.rb
+++ backend/app/controllers/api/v1/registrations_controller.rb
@@ -1,4 +1,18 @@
 class Api::V1::RegistrationsController < ApplicationController
   def signup
+    @user = User.new(registrations_params)
+
+    if @user.save
+      login!
+      render json: { status: :created, user: @user }
+    else
+      render json: { status: 500 }
+    end
+  end
+
+  private
+
+  def registrations_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
