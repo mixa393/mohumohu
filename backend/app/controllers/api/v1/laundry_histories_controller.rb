c7bd3c2a95410245f4ca86dfd083d0e8db70f286
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-03 18:01:24 +0900

    Add: laundry_histories_controller index,show,create,destroy

---
+++ backend/app/controllers/api/v1/laundry_histories_controller.rb
@@ -0,0 +1,58 @@
+class LaundryHistoriesController < ApplicationController
+
+  #ユーザーの所属しているチームに属する洗濯物の履歴を全件取得する
+  # @params [Integer] user_id
+  # @return [json] status,data
+  def index
+    laundry_histories = []
+
+    # ユーザーの所属チームを取得
+    team_id = User.find(params[:user_id]).team_id
+
+    # 所属チームの洗濯物を取得
+    laundries = Laundry.where(deleted_at: nil, team_id: team_id)
+
+    # 所属チームの洗濯物の履歴を全件表示
+    laundries.each do
+      laundry_history = LaundryHistory.where(deleted_at: nil, laundry_id: params[:laundry_id])
+      laundry_histories << laundry_history
+    end
+
+    render json: { status: 200, data: laundry_histories }
+  end
+
+  # １件の洗濯物についての履歴を取得
+  # @params [Integer] laundry_id
+  # @return [json] status,data
+  def show
+    laundry_histories = LaundryHistory.where(deleted_at: nil, laundry_id: params[:laundry_id])
+    render json: { status: 200, data: laundry_histories }
+  end
+
+  # 洗濯履歴を作成
+  # @params [Integer] user_id,laundry_id
+  # @return [json] status,data
+  def create
+    laundry_history = LaundryHistory.new(user_id: params[:user_id], laundry_id: params[:laundry_id])
+
+    if laundry_history.save
+      render json: { status: 200, data: laundry_history }
+    else
+      render json: { status: 400, message: "洗濯履歴作成に失敗しました", data: laundry_history.errors }
+    end
+  end
+
+  # 洗濯履歴を論理削除
+  # 削除できるのは自分で洗濯したもののみ
+  # @params [Integer] user_id,laundry_id
+  # @return [json] status,data
+  def destroy
+    laundry_history = LaundryHistory.where(deleted_at: nil, user_id: params[:user_id], laundry_id: params[:laundry_id])
+
+    if laundry_history.update(deleted_at: Time.now)
+      render json: { status: 200, data: laundry_history }
+    else
+      render json: { status: 400, message: "履歴削除に失敗しました", data: laundry_history.errors }
+    end
+  end
+end
--- backend/app/controllers/laundry_histories_controller.rb
+++
@@ -1,2 +0,0 @@
-class LaundryHistoriesController < ApplicationController
-end
