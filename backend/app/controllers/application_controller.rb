57d6492d8ec233bf593b04f5f92aa103512b896a
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-04 20:47:53 +0900

    Add: セッションにチームIDを保存&チームを取得するインスタンス変数を定義

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,17 +1,24 @@
 class ApplicationController < ActionController::API
+  helper_method :login!, :current_user, :current_team
   before_action :check_xhr_header
-
-  helper_method :login!, :current_user
 
   # セッションを使用してユーザーログイン
   def login!
     session[:user_id] = @user.id
+    session[:team_id] = @user.team_id
   end
 
-  # ログイン中のユーザーを取得するインスタンス変数を定義
+  # ログイン中のユーザーを取得するインスタンス変数
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
+    end
+  end
+
+  # チームを取得するインスタンス変数
+  def current_team
+    if session[:team_id]
+      @current_team ||= Team.find(session[:team_id])
     end
   end
 
