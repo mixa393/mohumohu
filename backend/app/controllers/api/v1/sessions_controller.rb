660b53f7dd1c14598ee46d12bb3172bdc5fad10b
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-05 18:36:49 +0900

    Fix: ログイン後cookieに保存、コメント追加

--- backend/app/controllers/api/v1/sessions_controller.rb
+++ backend/app/controllers/api/v1/sessions_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::SessionsController < ApplicationController
+  before_action :current_user, :current_team, only: [:logout]
 
   # ログイン処理
   # 入力したpassword/password_confirmationと、DBのpassword_digestの値を比較
   # 合致した場合にtrue
   def login
     user = User.where(deleted_at: nil).find_by(email: session_params[:email])
+    remember user
 
     if user && user.authenticate(session_params[:password])
       login! user
@@ -17,13 +19,21 @@ class Api::V1::SessionsController < ApplicationController
   # ログアウト処理
   # ログインしていれば動作可能
   def logout
-    unless login?
-      reset_session
-      forget(@current_user)
-      @current_user = nil
-      @current_team = nil
-      render json: { status: 200, logged_out: true }
-    end
+    # セッションを破棄
+    reset_session
+
+    # remember_digestを破棄
+    @current_user.forget
+
+    # cookiesを破棄
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+
+    # @current...を破棄
+    @current_user = nil
+    @current_team = nil
+
+    render json: { status: 200, logged_out: true }
   end
 
   private
