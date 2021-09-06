4b231746f5bc66ed8234b3adf4c1bcb426016d36
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-06 18:44:34 +0900

    FIx: セッション用の記述は消さないんでした

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -3,6 +3,24 @@ class ApplicationController < ActionController::API
   before_action :check_xhr_header
   include ActionController::Cookies
 
+
+  # セッション用の記述 ここから
+  helper_method :login!, :current_user
+
+  # セッションを使用してユーザーログイン
+  def login!
+    session[:user_id] = @user.id
+  end
+
+  # ログイン中のユーザーを取得するインスタンス変数を定義
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
+
+  # セッション用の記述 ここまで
+
   private
 
   def check_xhr_header
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get "/weather", to: "weather#get"
+      post '/login', to: 'sessions#login'
+      post '/logout', to: 'sessions#logout'
+      get '/logged_in', to: 'sessions#logged_in?'
+
       mount_devise_token_auth_for 'User', at: 'auth'
+
+      get "/weather", to: "weather#get"
+
       resources :teams, only: [:show, :create, :update, :destroy]
       resources :users, only: [:show, :create, :update, :destroy]
       resources :laundries
