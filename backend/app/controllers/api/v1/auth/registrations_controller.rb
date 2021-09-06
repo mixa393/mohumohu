5c656411bc6026928625cdadea578b2ecf579e4a
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-06 19:03:36 +0900

    Add: サインアップ用コントローラとルーティング

---
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -0,0 +1,12 @@
+# ユーザー登録用コントローラ
+class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  private
+
+  def sign_up_params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.permit(:name, :email)
+  end
+end
