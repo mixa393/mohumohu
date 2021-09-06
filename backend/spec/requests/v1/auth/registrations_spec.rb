4d9bd37baeb217673a6207c73e3259484c07e269
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-06 19:54:38 +0900

    Fix: サインインのテスト

--- backend/spec/requests/v1/auth/registrations_spec.rb
+++ backend/spec/requests/v1/auth/registrations_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "API::V1::Registrations", type: :request do
   let(:header) { { "X-Requested-With" => "XMLHttpRequest" } }
-  let!(:team) { FactoryBot.create(:team) }
 
-  it "POST /api/v1/auth" do
+  context "POST /api/v1/auth サインアップ" do
+    team = FactoryBot.create(:team)
+    header =  { "X-Requested-With" => "XMLHttpRequest" }
     password = Faker::Internet.password
     params = { name: Faker::Internet.username,
                email: Faker::Internet.unique.email,
@@ -12,9 +13,28 @@ RSpec.describe "API::V1::Registrations", type: :request do
                password_confirmation: password,
                team_id: team.id
     }
-    post "/api/v1/auth", headers: header, params: params
-    json = JSON.parse(response.body)
-    expect(json['data']['name']).to eq(params[:name])
-    expect(response.status).to eq(200)
+
+    it '200 ok' do
+      post "/api/v1/auth", headers: header, params: params
+      expect(response.status).to eq(200)
+    end
+
+    it '特定データの作成' do
+      post "/api/v1/auth", headers: header, params: params
+      json = JSON.parse(response.body)
+      expect(json['data']['name']).to eq(params[:name])
+    end
+  end
+
+  context 'POST api/v1/auth/sign_in サインイン' do
+    header =  { "X-Requested-With" => "XMLHttpRequest" }
+    user = FactoryBot.create(:user)
+    params = { email: user.email, password: user.password }
+
+    it '200 ok' do
+      post "/api/v1/auth/sign_in", headers: header, params: params
+      expect(response.status).to eq(200)
+    end
   end
 end
+
