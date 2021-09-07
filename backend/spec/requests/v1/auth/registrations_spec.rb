cccd7bf19b2d81f271b2d3ba5b6ee474305fbe92
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-07 12:05:11 +0900

    Fix: テストのリファクタリング

--- backend/spec/requests/v1/auth/registrations_spec.rb
+++ backend/spec/requests/v1/auth/registrations_spec.rb
@@ -1,35 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "API::V1::Registrations", type: :request do
-  let(:header) { { "X-Requested-With" => "XMLHttpRequest" } }
 
   context "POST /api/v1/auth サインアップ" do
-    team = FactoryBot.create(:team)
-    header = { "X-Requested-With" => "XMLHttpRequest" }
-    password = Faker::Internet.password
-    params = { name: Faker::Internet.username,
-               email: Faker::Internet.unique.email,
-               password: password,
-               password_confirmation: password,
-               team_id: team.id
-    }
+    let!(:team) { FactoryBot.create(:team) }
+    let!(:header) { { "X-Requested-With" => "XMLHttpRequest" } }
+    let(:password) { Faker::Internet.password }
+    let(:params) { { name: Faker::Internet.username,
+                     email: Faker::Internet.unique.email,
+                     password: password,
+                     password_confirmation: password,
+                     team_id: team.id } }
+    before do
+      post "/api/v1/auth", headers: header, params: params
+    end
 
     it '200 ok' do
-      post "/api/v1/auth", headers: header, params: params
       expect(response.status).to eq(200)
     end
 
     it '特定データの作成' do
-      post "/api/v1/auth", headers: header, params: params
       json = JSON.parse(response.body)
       expect(json['data']['name']).to eq(params[:name])
     end
   end
 
   context 'POST api/v1/auth/sign_in サインイン' do
-    header = { "X-Requested-With" => "XMLHttpRequest" }
-    user = FactoryBot.create(:user)
-    params = { email: user.email, password: user.password }
+    let(:header) { { "X-Requested-With" => "XMLHttpRequest" } }
+    let!(:user) { FactoryBot.create(:user) }
+    let(:params) { { email: user.email, password: user.password } }
 
     it '200 ok' do
       post "/api/v1/auth/sign_in", headers: header, params: params
