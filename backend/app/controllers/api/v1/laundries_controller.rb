b89a3015c7bb956e922e20d1076e1cd264bee970
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-07 16:36:17 +0900

    Fix: LaundriesController 認証を組み込み

--- backend/app/controllers/api/v1/laundries_controller.rb
+++ backend/app/controllers/api/v1/laundries_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::LaundriesController < ApplicationController
-  before_action :login?
+  before_action :authenticate_api_v1_user!
   before_action :set_laundry, only: [:show, :update, :destroy]
 
   def show
