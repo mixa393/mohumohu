7c24fa486f4b9df8616cbf5d8f39b71ac520a9e6
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-07 17:26:18 +0900

    Fix: registrations#destroyを論理削除にオーバーライド

--- backend/app/controllers/api/v1/auth/registrations_controller.rb
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,5 +1,25 @@
 # ユーザー登録用コントローラ
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+
+  # registraions#destroy のオーバーライド
+  # 論理削除 deleted_atカラムを現在日時に更新
+  # @return [json] status,data,message
+  def destroy
+    if @resource
+      @resource.update(deleted_at: Time.now)
+      yield @resource if block_given?
+
+      # 以下はrender_destroy_messageにdataを加えたもの
+      render json: {
+        status: 'success',
+        data: @resource,
+        message: I18n.t('devise_token_auth.registrations.account_with_uid_destroyed', uid: @resource.uid)
+      }
+    else
+      render_destroy_error
+    end
+  end
+
   private
 
   def sign_up_params
--- backend/spec/requests/v1/auth/registrations_spec.rb
+++ backend/spec/requests/v1/auth/registrations_spec.rb
@@ -39,18 +39,18 @@ RSpec.describe "API::V1::Registrations", type: :request do
   context "PUT api/v1/auth/password パスワードの変更" do
     include AuthorizationSpecHelper
     let!(:user) { FactoryBot.create(:user) }
-    let(:update_headers) { sign_in(user) }
+    let(:auth_tokens) { sign_in(user) }
     let(:password) { Faker::Internet.password }
     let(:update_params) { { "password" => password, "password_confirmation" => password } }
 
     it '200 ok' do
       # パスワード変更のリクエスト
-      put "/api/v1/auth/password", headers: update_headers, params: update_params
+      put "/api/v1/auth/password", headers: auth_tokens, params: update_params
       expect(response.status).to eq(200)
     end
 
     it '更新に成功' do
-      put "/api/v1/auth/password", headers: update_headers, params: update_params
+      put "/api/v1/auth/password", headers: auth_tokens, params: update_params
       json = JSON.parse(response.body)
       expect(json["message"]).to include("更新に成功")
     end
@@ -59,19 +59,36 @@ RSpec.describe "API::V1::Registrations", type: :request do
   context "PUT api/v1/auth ユーザー情報の変更" do
     include AuthorizationSpecHelper
     let!(:user) { FactoryBot.create(:user) }
-    let(:update_headers) { sign_in(user) }
+    let(:auth_tokens) { sign_in(user) }
     let(:update_params) { { "name" => Faker::Internet.username, "email" => Faker::Internet.unique.email } }
 
     it '200 ok' do
-      put "/api/v1/auth", headers: update_headers, params: update_params
+      put "/api/v1/auth", headers: auth_tokens, params: update_params
       expect(response.status).to eq(200)
     end
 
     it '特定データに変更' do
-      put "/api/v1/auth", headers: update_headers, params: update_params
+      put "/api/v1/auth", headers: auth_tokens, params: update_params
       json = JSON.parse(response.body)
       expect(json["data"]["name"]).to eq(update_params["name"])
       expect(json["data"]["email"]).to eq(update_params["email"])
+    end
+  end
+
+  context "DELETE api/v1/auth ユーザー情報の論理削除" do
+    include AuthorizationSpecHelper
+    let!(:user) { FactoryBot.create(:user) }
+    let(:auth_tokens) { sign_in(user) }
+
+    it '200 ok' do
+      delete "/api/v1/auth", headers: auth_tokens
+      expect(response.status).to eq(200)
+    end
+
+    it 'deleted_atの更新' do
+      delete "/api/v1/auth", headers: auth_tokens
+      json = JSON.parse(response.body)
+      expect(json["data"]["deleted_at"]).not_to eq(nil)
     end
   end
 end
