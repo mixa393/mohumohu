4d511343bf176c0c58c3cf91cf8c16f6a77330b6
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-07 11:19:09 +0900

    Error: undefined method `merge!' for nil:NilClass (NoMethodError)

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -41,6 +41,5 @@ module Myapp
 
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
   end
 end
