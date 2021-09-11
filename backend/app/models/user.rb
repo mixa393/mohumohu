c3b060f2b21548d3ca00c322157e96120312caaa
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-11 19:57:22 +0900

    Fix: Userモデルのパスワード null不許可バリデーションの追加(devise入れた時消していた模様)

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   has_many :laundries
 
   validates :name, presence: true, length: { maximum: 31 }
+  validates :password, presence: true, length: { minimum: 6, maximum: 255 }
+  validates :password_confirmation, presence: true, length: { minimum: 6, maximum: 255 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }, length: { maximum: 127 }
