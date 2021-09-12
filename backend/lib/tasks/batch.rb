5fba013b4b3de91949001d5f0baceeddbf143cde
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-12 20:33:56 +0900

    Fix: 実行処理修正

--- backend/lib/tasks/batch.rb
+++ backend/lib/tasks/batch.rb
@@ -1,16 +1,21 @@
 module Batch
 
   def update_wash_at
-    today = Time.now.to_date
+    # 昨日の日時
+    yesterday = Time.now.to_date - 1
 
-    # wash_atが今日のものを取得
-    laundries = Laundry.where(deleted_at: nil, wash_at: today)
+    # wash_atが昨日のものを取得
+    laundries = Laundry.where(deleted_at: nil, wash_at: yesterday)
 
     # その全てのwash_atを、今日からdays日後に修正し直す
     laundries.each do |laundry|
 
       # controller風に書けるなら以下
-      laundry.update(wash_at: laundry.wash_at + laundry.days)
+      if laundry.days
+        laundry.update(wash_at: laundry.wash_at + laundry.days)
+      else
+        laundry.update(wash_at: laundry.wash_at + 30)
+      end
 
       # API実行なら以下
       # put "/api/v1/laundries/#{laundry.id}", params: { wash_at: laundry.wash_at + laundry.days }
