a374429be0bcf2b6550dbc62a5afc22a1bdaea41
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-12 18:52:49 +0900

    Fix: showメソッドのチーム制限やり直し

--- backend/app/controllers/api/v1/laundry_histories_controller.rb
+++ backend/app/controllers/api/v1/laundry_histories_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::LaundryHistoriesController < ApplicationController
   before_action :authenticate_api_v1_user!
-  before_action :current_team, only: [:index]
+  before_action :current_team, only: [:index, :show]
 
   #ユーザーの所属しているチームに属する洗濯物の履歴を全件取得する
   # @return [json] status,data(Array)
@@ -23,8 +23,25 @@ class Api::V1::LaundryHistoriesController < ApplicationController
   # @params [Integer] laundry_id,URLから取得
   # @return [json] status,data(Array)
   def show
-    laundry_histories = LaundryHistory.where(deleted_at: nil, team_id: @current_team.id, laundry_id: params[:id])
-    render json: { status: 200, data: laundry_histories }
+    # URLからIDを取得
+    laundry_id = params[:id].to_i
+
+    # 同チームに属する洗濯物一覧を取得
+    laundries = Laundry.where(deleted_at: nil, team_id: @current_team.id)
+
+    # 同チームの洗濯物一覧のIDを配列にする
+    laundry_ids = []
+    laundries.each do |laundry|
+      laundry_ids << laundry.id
+    end
+
+    # 洗濯物IDが洗濯物一覧のいずれかのIDと合致したら値を返す
+    if laundry_ids.include?(laundry_id)
+      laundry_histories = LaundryHistory.where(deleted_at: nil, laundry_id: params[:id])
+      render json: { status: 200, data: laundry_histories }
+    else
+      render json: { status: 400, message: "洗濯履歴取得に失敗しました" }
+    end
   end
 
   # 洗濯履歴を作成
