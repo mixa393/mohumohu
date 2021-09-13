bf09ef523de206f1ba2f59aa6080acf18b963ec5
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-13 16:55:36 +0900

    Fix: rootの書き方が間違ってたっぽい

--- backend/config/schedule.rb
+++ backend/config/schedule.rb
@@ -1,5 +1,5 @@
 # Rails.rootを使用するために必要
-require File.expand_path(File.dirname(FILE) + "/environment")
+require File.expand_path(File.dirname(__FILE__) + "/environment")
 
 # cronのログの吐き出し場所
 set :output, "#{Rails.root}/log/cron.log"
@@ -11,7 +11,7 @@ rails_env = ENV['RAILS_ENV'] || :development
 set :environment, rails_env
 
 every 1.days do
-  rake "batch:update_wash_at"
+  rake "laundry:update_wash_at"
 end
 
 # Use this file to easily define all of your cron jobs.
