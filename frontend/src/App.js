7d10491435c386df830477ccd60eeb567f400ae4
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-13 20:50:26 +0900

    Fix: グローバルで扱う変数の追加

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import {useEffect, useState} from "react";
+import {useEffect, useState, createContext} from "react";
 import {
     BrowserRouter,
     Switch,
     Route,
-    useLocation
+    Redirect
 } from "react-router-dom";
 import './css/App.css';
 
@@ -11,16 +11,21 @@ import Header from './components/common/header'
 import Footer from './components/common/footer'
 import UsersIndex from "./pages/users/index";
 import LaundriesIndex from "./pages/laundries";
+import SignIn from "./pages/signIn";
 
 import {getCurrentUser} from "./lib/api/auth";
 
-function App() {
-    const location = useLocation();
-    useEffect(() => {
-        return () => {
 
-        };
-    }, [location]);
+/**
+ * グローバルで扱う変数・関数
+ * @type {React.Context<{isSignedIn, setIsSignedIn, currentUser, setCurrentUser}>}
+ */
+export const AuthContext = createContext({
+    isSignedIn: undefined, setIsSignedIn: undefined, currentUser: undefined, setCurrentUser: undefined
+})
+
+function App() {
+
     const [isSignedIn, setIsSignedIn] = useState(false)
     const [currentUser, setCurrentUser] = useState("")
 
@@ -42,55 +47,60 @@ function App() {
             console.log(err)
         }
 
-        setLoading(false)
+        // setLoading(false)
     }
 
     useEffect(() => {
-        handleGetCurrentUser()
+        handleGetCurrentUser().then()
     }, [setCurrentUser])
 
 
     // ユーザーが認証済みかどうかでルーティングを決定
     // 未認証だった場合は「/signin」ページに促す
-    const Private = ({ children }) => {
-        if (!loading) {
-            if (isSignedIn) {
-                return children
-            } else {
-                return <Redirect to="/signin" />
-            }
+    const Private = ({children}) => {
+        // if (!loading) {
+        if (isSignedIn) {
+            return children
         } else {
-            return <></>
+            return <Redirect to="/signin"/>
         }
+        // } else {
+        //     return <></>
+        // }
     }
 
     return (
         <BrowserRouter>
-            <div className="App flex flex-col">
-                <Header/>
-                <div className="flex-1">
-                    <Switch>
-                        <Route exact path="/">
-                            <UsersIndex/>
-                        </Route>
-                        <Route path="/laundries">
-                            <LaundriesIndex/>
-                        </Route>
-                        <Route path="/settings">
-                            <div>設定</div>
-                        </Route>
-                        {/*TODO:認証後では関連*/}
-                        <Route path="/login">
-                            <div>ログイン画面予定</div>
-                        </Route>
-                        <Route path="/logout">
-                            <div>ログアウト予定</div>
-                        </Route>
-
-                    </Switch>
+            <AuthContext.Provider value={{isSignedIn, setIsSignedIn, currentUser, setCurrentUser}}>
+                <div className="App flex flex-col">
+                    <Header/>
+                    <div className="flex-1">
+                        <Switch>
+                            <Route exact path="/signup" component={SignUp}/>
+                            <Route exact path="/signin" component={SignIn}/>
+                            <Private>
+                                <Route exact path="/">
+                                    <UsersIndex/>
+                                </Route>
+                                <Route path="/laundries">
+                                    <LaundriesIndex/>
+                                </Route>
+                                <Route path="/settings">
+                                    <div>設定</div>
+                                </Route>
+                                {/*TODO:認証後では関連*/}
+                                <Route path="/login">
+                                    <div>ログイン画面予定</div>
+                                </Route>
+                                <Route path="/logout">
+                                    <div>ログアウト予定</div>
+                                </Route>
+                            </Private>
+                        </Switch>
+                    </div>
+                    <Footer/>
                 </div>
-                <Footer/>
-            </div>
+            </AuthContext.Provider>
         </BrowserRouter>
     );
 }
