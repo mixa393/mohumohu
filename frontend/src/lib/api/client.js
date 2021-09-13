49b6e87efe33136c27fb6479a838b6cf7ba4018c
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-13 19:48:38 +0900

    Fix: キャメルケースとスネークケースの変換

--- frontend/src/lib/api/client.js
+++ frontend/src/lib/api/client.js
@@ -1,8 +1,13 @@
 import applyCaseMiddleware from "axios-case-converter"
 import axios from "axios"
 
-const client = axios.create({
+// ヘッダーに関してはケバブケースのままで良いので適用を無視するオプションを追加
+const options = {
+    ignoreHeaders: true
+}
+
+const client = applyCaseMiddleware(axios.create({
     baseURL: "http://localhost:3001/api/v1"
-})
+}), options)
 
 export default client
