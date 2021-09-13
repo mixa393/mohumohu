017e597dea7cd2096a1a8c4de50d1c31dfd9ce9f
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-13 14:48:39 +0900

    Fix: rakeタスクからインスタンスメソッドの呼び出し

--- backend/app/models/laundry.rb
+++ backend/app/models/laundry.rb
@@ -17,21 +17,21 @@ class Laundry < ApplicationRecord
     errors.add(:wash_at, "は現在の日時より遅い時間を選択してください") if self.wash_at < Time.now.to_date
   end
 
-  # def update_wash_at
-  #   # 昨日の日時
-  #   yesterday = Time.now.to_date - 1
-  #
-  #   # wash_atが昨日のものを取得
-  #   laundries = Laundry.where(deleted_at: nil, wash_at: yesterday)
-  #
-  #   # その全てのwash_atを、今日からdays日後に修正し直す
-  #   laundries.each do |laundry|
-  #
-  #     if laundry.days
-  #       laundry.update(wash_at: laundry.wash_at + laundry.days)
-  #     else
-  #       laundry.update(wash_at: laundry.wash_at + 30)
-  #     end
-  #   end
-  # end
+  def update_wash_at
+    # 昨日の日時
+    yesterday = Time.now.to_date - 1
+
+    # wash_atが昨日のものを取得
+    laundries = Laundry.where(deleted_at: nil, wash_at: yesterday)
+
+    # その全てのwash_atを、今日からdays日後に修正し直す
+    laundries.each do |laundry|
+
+      if laundry.days
+        laundry.update(wash_at: laundry.wash_at + laundry.days)
+      else
+        laundry.update(wash_at: laundry.wash_at + 30)
+      end
+    end
+  end
 end
--- backend/lib/tasks/batch/laundries.rake
+++ backend/lib/tasks/batch/laundries.rake
@@ -1,20 +1,6 @@
 namespace :laundries do
   desc "毎日wash_atが昨日のものの値をdays日後に更新する"
   task update_wash_at: :environment do
-    # 昨日の日時
-    yesterday = Time.now.to_date - 1
-
-    # wash_atが昨日のものを取得
-    laundries = Laundry.where(deleted_at: nil, wash_at: yesterday)
-
-    # その全てのwash_atを、今日からdays日後に修正し直す
-    laundries.each do |laundry|
-
-      if laundry.days
-        laundry.update(wash_at: laundry.wash_at + laundry.days)
-      else
-        laundry.update(wash_at: laundry.wash_at + 30)
-      end
-    end
+    Laundry.new.update_wash_at
   end
 end
