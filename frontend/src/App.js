7f98cdc342937a31b42d462cf20643ee495cf357
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-13 19:51:39 +0900

    Fix: import整理とlocation追加

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
+import {useEffect, useState} from "react";
 import {
     BrowserRouter,
     Switch,
     Route,
+    useLocation
 } from "react-router-dom";
 import './css/App.css';
+
 import Header from './components/common/header'
 import Footer from './components/common/footer'
 import UsersIndex from "./pages/users/index";
@@ -12,6 +15,55 @@ import LaundriesIndex from "./pages/laundries";
 import {getCurrentUser} from "./lib/api/auth";
 
 function App() {
+    const location = useLocation();
+    useEffect(() => {
+        return () => {
+
+        };
+    }, [location]);
+    const [isSignedIn, setIsSignedIn] = useState(false)
+    const [currentUser, setCurrentUser] = useState("")
+
+    // 認証済みのユーザーがいるかどうかチェック
+    // 確認できた場合はそのユーザーの情報を取得
+    const handleGetCurrentUser = async () => {
+        try {
+            const res = await getCurrentUser()
+
+            if (res?.data.isLogin === true) {
+                setIsSignedIn(true)
+                setCurrentUser(res?.data.data)
+
+                console.log(res?.data.data)
+            } else {
+                console.log("No current user")
+            }
+        } catch (err) {
+            console.log(err)
+        }
+
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        handleGetCurrentUser()
+    }, [setCurrentUser])
+
+
+    // ユーザーが認証済みかどうかでルーティングを決定
+    // 未認証だった場合は「/signin」ページに促す
+    const Private = ({ children }) => {
+        if (!loading) {
+            if (isSignedIn) {
+                return children
+            } else {
+                return <Redirect to="/signin" />
+            }
+        } else {
+            return <></>
+        }
+    }
+
     return (
         <BrowserRouter>
             <div className="App flex flex-col">
