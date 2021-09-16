fb0f6bbdfcfc2d721c7539227b90bd1eb9bedb88
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-16 16:02:51 +0900

    Fix: showの異なる履歴を取得できないテスト

--- backend/spec/requests/v1/laundry_histories_spec.rb
+++ backend/spec/requests/v1/laundry_histories_spec.rb
@@ -47,89 +47,42 @@ RSpec.describe "LaundryHistoriesAPI", type: :request do
     context "同じチームの履歴がある場合" do
       let(:team) { FactoryBot.create(:team) }
       let(:users) { FactoryBot.create_list(:user, 2, team_id: team.id) }
-      let(:auth_tokens) { sign_in(users.first) }
       let(:laundry) { FactoryBot.create(:laundry, user_id: users.second.id, team_id: team.id) }
       before :each do
         FactoryBot.create(:laundry_history, user_id: users.first.id, laundry_id: laundry.id)
         FactoryBot.create(:laundry_history, user_id: users.last.id, laundry_id: laundry.id)
       end
 
+      let(:auth_tokens) { sign_in(users.first) }
+
       it "履歴を取得できていること" do
         get "/api/v1/laundry_histories/#{laundry.id}", headers: auth_tokens
-
         expect(response.status).to eq(200)
-        json = JSON.parse(response.body)
 
+        json = JSON.parse(response.body)
         expect(json['data'].length).to eq(2)
       end
     end
 
     context "違うチームの履歴がある場合" do
+      let(:user) { FactoryBot.create(:user) }
+      let(:auth_tokens) { sign_in(user) }
+
+      # 異なるチームに属する洗濯物
+      let(:laundry) { FactoryBot.create(:laundry) }
+      before :each do
+        FactoryBot.create(:laundry_history, laundry_id: laundry.id)
+      end
+
+      it '履歴を取得できないこと' do
+        get "/api/v1/laundry_histories/#{laundry.id}", headers: auth_tokens
+        expect(response.status).to eq(200)
+
+        json = JSON.parse(response.body)
+        expect(json['data']).to eq([])
+      end
 
     end
+
   end
-  #
-  # describe "POST /api/v1/laundry_histories"  do
-  #   let(:user) { FactoryBot.create(:user) }
-  #   let(:laundry) { FactoryBot.create(:laundry, user_id: user.id, team_id: user.team_id) }
-  #
-  #   let(:auth_tokens) { sign_in(user) }
-  #
-  #   it "洗濯履歴の作成" do
-  #     expect { post "/api/v1/laundry_histories", headers: auth_tokens, params: { laundry_id: laundry.id } }
-  #       .to change(LaundryHistory, :count).by(+1)
-  #
-  #     expect(response.status).to eq(200)
-  #   end
-  # end
-  #
-  # describe "DELETE /api/v1/laundry_histories/:id" do
-  #   # サインイン
-  #   let(:user) { FactoryBot.create(:user) }
-  #   let(:auth_tokens) { sign_in(user) }
-  #
-  #   let(:laundry) { FactoryBot.create(:laundry, user_id: user.id, team_id: user.team_id) }
-  #   let!(:laundry_history){ FactoryBot.create(:laundry_history, user_id: user.id, laundry_id: laundry.id)}
-  #
-  #   it "DELETE /api/v1/laundry_histories/:id" do
-  #     delete "/api/v1/laundry_histories/#{laundry_history.id}", headers: auth_tokens
-  #
-  #     json = JSON.parse(response.body)
-  #     expect(json['data']['deleted_at']).not_to eq(nil)
-  #     expect(response.status).to eq(200)
-  #   end
-  # end
-
-  # context do
-  #   let!(:user) { FactoryBot.create(:user) }
-  #   let!(:laundry) { FactoryBot.create(:laundry, user_id: user.id, team_id: user.team_id) }
-  #   let!(:laundry_history){ FactoryBot.create(:laundry_history, user_id: user.id, laundry_id: laundry.id)}
-  #
-  #   let(:auth_tokens) { sign_in(user) }
-  #   let(:json) { JSON.parse(response.body) }
-
-  # it "GET /api/v1/laundry_histories/:id" do
-  #   get "/api/v1/laundry_histories/#{laundry.id}", headers: auth_tokens
-  #
-  #   expect(response.status).to eq(200)
-  #
-  #   if json["data"].first
-  #     expect(json['data'].first["laundry_id"]).to eq(laundry.id)
-  #   end
-  # end
-  #
-  # it "POST /api/v1/laundry_histories" do
-  #   expect { post "/api/v1/laundry_histories", headers: auth_tokens, params: { laundry_id: laundry.id } }
-  #     .to change(LaundryHistory, :count).by(+1)
-  #
-  #   expect(response.status).to eq(200)
-  # end
-  #
-  #   it "DELETE /api/v1/laundry_histories/:id" do
-  #     delete "/api/v1/laundry_histories/#{laundry_history.id}", headers: auth_tokens
-  #
-  #     expect(json['data']['deleted_at']).not_to eq(nil)
-  #     expect(response.status).to eq(200)
-  #   end
-  # end
 end
