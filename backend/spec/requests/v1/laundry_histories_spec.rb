3441b7dbc6b8ff84d1e9fd0ee0ebbb895241d72d
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-16 15:46:10 +0900

    Fix: showの同じチームの場合テスト

--- backend/spec/requests/v1/laundry_histories_spec.rb
+++ backend/spec/requests/v1/laundry_histories_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe "LaundryHistoriesAPI", type: :request do
 
     context '同じチームの洗濯履歴がある場合' do
       # 同じチームに属するデータを作成
-      let!(:team) { FactoryBot.create(:team) }
-      let!(:users) { FactoryBot.create_list(:user, 2, team_id: team.id) }
-      let!(:laundries) { FactoryBot.create_list(:laundry, 2, team_id: team.id) }
+      let(:team) { FactoryBot.create(:team) }
+      let(:users) { FactoryBot.create_list(:user, 2, team_id: team.id) }
+      let(:laundries) { FactoryBot.create_list(:laundry, 2, team_id: team.id) }
       before :each do
         FactoryBot.create(:laundry_history, user_id: users.first.id, laundry_id: laundries.first.id)
         FactoryBot.create(:laundry_history, user_id: users.last.id, laundry_id: laundries.last.id)
@@ -25,10 +25,10 @@ RSpec.describe "LaundryHistoriesAPI", type: :request do
     end
 
     context '違うチームの履歴がある場合' do
-      let!(:users) { FactoryBot.create_list(:user, 2) }
+      let(:users) { FactoryBot.create_list(:user, 2) }
 
-      let!(:first_laundry) { FactoryBot.create(:laundry, user_id: users.first.id, team_id: users.first.team_id) }
-      let!(:second_laundry) { FactoryBot.create(:laundry, user_id: users.last.id) }
+      let(:first_laundry) { FactoryBot.create(:laundry, user_id: users.first.id, team_id: users.first.team_id) }
+      let(:second_laundry) { FactoryBot.create(:laundry, user_id: users.last.id) }
 
       before :each do
         FactoryBot.create(:laundry_history, user_id: users.first.id, laundry_id: first_laundry.id)
@@ -43,36 +43,93 @@ RSpec.describe "LaundryHistoriesAPI", type: :request do
     end
   end
 
-  context do
-    let!(:user) { FactoryBot.create(:user) }
-    let!(:laundry) { FactoryBot.create(:laundry, user_id: user.id, team_id: user.team_id) }
-    let!(:laundry_history){ FactoryBot.create(:laundry_history, user_id: user.id, laundry_id: laundry.id)}
+  describe "GET /api/v1/laundry_histories/:id" do
+    context "同じチームの履歴がある場合" do
+      let(:team) { FactoryBot.create(:team) }
+      let(:users) { FactoryBot.create_list(:user, 2, team_id: team.id) }
+      let(:auth_tokens) { sign_in(users.first) }
+      let(:laundry) { FactoryBot.create(:laundry, user_id: users.second.id, team_id: team.id) }
+      before :each do
+        FactoryBot.create(:laundry_history, user_id: users.first.id, laundry_id: laundry.id)
+        FactoryBot.create(:laundry_history, user_id: users.last.id, laundry_id: laundry.id)
+      end
 
-    let(:auth_tokens) { sign_in(user) }
-    let(:json) { JSON.parse(response.body) }
+      it "履歴を取得できていること" do
+        get "/api/v1/laundry_histories/#{laundry.id}", headers: auth_tokens
 
-    it "GET /api/v1/laundry_histories/:id" do
-      get "/api/v1/laundry_histories/#{laundry.id}", headers: auth_tokens
+        expect(response.status).to eq(200)
+        json = JSON.parse(response.body)
 
-      expect(response.status).to eq(200)
-
-      if json["data"].first
-        expect(json['data'].first["laundry_id"]).to eq(laundry.id)
+        expect(json['data'].length).to eq(2)
       end
     end
 
-    it "POST /api/v1/laundry_histories" do
-      expect { post "/api/v1/laundry_histories", headers: auth_tokens, params: { laundry_id: laundry.id } }
-        .to change(LaundryHistory, :count).by(+1)
+    context "違うチームの履歴がある場合" do
 
-      expect(response.status).to eq(200)
-    end
-
-    it "DELETE /api/v1/laundry_histories/:id" do
-      delete "/api/v1/laundry_histories/#{laundry_history.id}", headers: auth_tokens
-
-      expect(json['data']['deleted_at']).not_to eq(nil)
-      expect(response.status).to eq(200)
     end
   end
+  #
+  # describe "POST /api/v1/laundry_histories"  do
+  #   let(:user) { FactoryBot.create(:user) }
+  #   let(:laundry) { FactoryBot.create(:laundry, user_id: user.id, team_id: user.team_id) }
+  #
+  #   let(:auth_tokens) { sign_in(user) }
+  #
+  #   it "洗濯履歴の作成" do
+  #     expect { post "/api/v1/laundry_histories", headers: auth_tokens, params: { laundry_id: laundry.id } }
+  #       .to change(LaundryHistory, :count).by(+1)
+  #
+  #     expect(response.status).to eq(200)
+  #   end
+  # end
+  #
+  # describe "DELETE /api/v1/laundry_histories/:id" do
+  #   # サインイン
+  #   let(:user) { FactoryBot.create(:user) }
+  #   let(:auth_tokens) { sign_in(user) }
+  #
+  #   let(:laundry) { FactoryBot.create(:laundry, user_id: user.id, team_id: user.team_id) }
+  #   let!(:laundry_history){ FactoryBot.create(:laundry_history, user_id: user.id, laundry_id: laundry.id)}
+  #
+  #   it "DELETE /api/v1/laundry_histories/:id" do
+  #     delete "/api/v1/laundry_histories/#{laundry_history.id}", headers: auth_tokens
+  #
+  #     json = JSON.parse(response.body)
+  #     expect(json['data']['deleted_at']).not_to eq(nil)
+  #     expect(response.status).to eq(200)
+  #   end
+  # end
+
+  # context do
+  #   let!(:user) { FactoryBot.create(:user) }
+  #   let!(:laundry) { FactoryBot.create(:laundry, user_id: user.id, team_id: user.team_id) }
+  #   let!(:laundry_history){ FactoryBot.create(:laundry_history, user_id: user.id, laundry_id: laundry.id)}
+  #
+  #   let(:auth_tokens) { sign_in(user) }
+  #   let(:json) { JSON.parse(response.body) }
+
+  # it "GET /api/v1/laundry_histories/:id" do
+  #   get "/api/v1/laundry_histories/#{laundry.id}", headers: auth_tokens
+  #
+  #   expect(response.status).to eq(200)
+  #
+  #   if json["data"].first
+  #     expect(json['data'].first["laundry_id"]).to eq(laundry.id)
+  #   end
+  # end
+  #
+  # it "POST /api/v1/laundry_histories" do
+  #   expect { post "/api/v1/laundry_histories", headers: auth_tokens, params: { laundry_id: laundry.id } }
+  #     .to change(LaundryHistory, :count).by(+1)
+  #
+  #   expect(response.status).to eq(200)
+  # end
+  #
+  #   it "DELETE /api/v1/laundry_histories/:id" do
+  #     delete "/api/v1/laundry_histories/#{laundry_history.id}", headers: auth_tokens
+  #
+  #     expect(json['data']['deleted_at']).not_to eq(nil)
+  #     expect(response.status).to eq(200)
+  #   end
+  # end
 end
