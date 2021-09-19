76f15d6f22d47046c7dd7c8c0be18d43426c5afb
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-19 16:56:45 +0900

    Fix: idが数字の時のみにルーティングを限定

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       get "/weather", to: "weather#get"
 
       resources :teams, only: [:show, :create, :update, :destroy]
-      resources :laundries
+      resources :laundries, constraints: { id: /\d+/ }
       get "/laundries/list", to: "laundries#list"
 
       # ログイン用
