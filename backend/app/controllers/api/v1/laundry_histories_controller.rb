bdc7541bdc2e5b6bebdfffbddc633c2817d3a292
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-20 20:04:19 +0900

    Add: laundryチェックのbefore_action

--- backend/app/controllers/api/v1/laundry_histories_controller.rb
+++ backend/app/controllers/api/v1/laundry_histories_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::LaundryHistoriesController < ApplicationController
   before_action :authenticate_api_v1_user!
   before_action :current_team, only: [:index, :show, :create]
+  before_action :laundry_check, only: [:create,:show]
 
   #ユーザーの所属しているチームに属する洗濯物の履歴を全件取得する
   # @return [json] status,data(Array)
@@ -23,24 +24,29 @@ class Api::V1::LaundryHistoriesController < ApplicationController
   # @params [Integer] laundry_id,URLから取得
   # @return [json] status,data(Array)
   def show
-    # URLからIDを取得
-    laundry_id = params[:id].to_i
+    # # URLからIDを取得
+    # laundry_id = params[:id].to_i
+    #
+    # # 同チームに属する洗濯物一覧を取得
+    # laundries = Laundry.where(deleted_at: nil, team_id: @current_team.id)
+    #
+    # # 同チームの洗濯物一覧のIDを配列にする
+    # laundry_ids = []
+    # laundries.each do |laundry|
+    #   laundry_ids << laundry.id
+    # end
+    #
+    # # 洗濯物IDが洗濯物一覧のいずれかのIDと合致したら値を返す
+    # if laundry_ids.include?(laundry_id)
+    #   laundry_histories = LaundryHistory.where(deleted_at: nil, laundry_id: params[:id])
+    #   render json: { status: 200, data: laundry_histories }
+    # else
+    #   render json: { status: 200, data: [] }
+    # end
 
-    # 同チームに属する洗濯物一覧を取得
-    laundries = Laundry.where(deleted_at: nil, team_id: @current_team.id)
-
-    # 同チームの洗濯物一覧のIDを配列にする
-    laundry_ids = []
-    laundries.each do |laundry|
-      laundry_ids << laundry.id
-    end
-
-    # 洗濯物IDが洗濯物一覧のいずれかのIDと合致したら値を返す
-    if laundry_ids.include?(laundry_id)
+    if @laundry
       laundry_histories = LaundryHistory.where(deleted_at: nil, laundry_id: params[:id])
       render json: { status: 200, data: laundry_histories }
-    else
-      render json: { status: 200, data: [] }
     end
   end
 
@@ -48,18 +54,7 @@ class Api::V1::LaundryHistoriesController < ApplicationController
   # @params [Integer] laundry_id,リクエストボディから取得
   # @return [json] status,data(Array)
   def create
-    laundry_id = params[:laundry_id]
-
-    begin
-      # 現チーム、削除されていない洗濯物かどうかチェック
-      Laundry.where(team_id: @current_team.id, deleted_at: nil).find(laundry_id)
-    rescue ActiveRecord::RecordNotFound
-      # 洗濯物IDが不正の場合messageだけ返却して抜ける
-      render json: { status: 400, message: "洗濯物IDが不正です"  }
-      return
-    end
-
-    laundry_history = LaundryHistory.new(user_id: current_api_v1_user.id, laundry_id: laundry_id)
+    laundry_history = LaundryHistory.new(user_id: current_api_v1_user.id, laundry_id: @laundry.id)
 
     if laundry_history.save
       render json: { status: 200, data: laundry_history }
@@ -86,6 +81,19 @@ class Api::V1::LaundryHistoriesController < ApplicationController
       render json: { status: 200, data: laundry_history }
     else
       render json: { status: 400, message: "履歴削除に失敗しました", data: laundry_history.errors }
+    end
+  end
+
+  private
+
+  # 現チーム、削除されていない洗濯物かどうかチェック
+  # 洗濯物IDが不正の場合messageだけ返却して抜ける
+  def laundry_check
+    begin
+      # 自分のチームに所属する洗濯物のみを取得可能
+      @laundry = Laundry.where(deleted_at: nil, team_id: current_api_v1_user.team_id).find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { status: 400, message: "データの取得に失敗しました" }
     end
   end
 end
