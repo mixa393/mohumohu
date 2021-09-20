13b348ec237023ddd1ebbd7f5e002ea43d4477d3
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-20 21:40:39 +0900

    Add: washed comment

--- backend/app/controllers/api/v1/laundries_controller.rb
+++ backend/app/controllers/api/v1/laundries_controller.rb
@@ -102,6 +102,10 @@ class Api::V1::LaundriesController < ApplicationController
     render json: { status: 200, data: @laundry }
   end
 
+  # 「洗濯した」用のメソッド
+  # wash_atを今日からdays日後に更新
+  # @params [Integer] laundry_id,URLから取得
+  # @return [json] 更新後wash_at(XX月YY日に変換)
   def washed
     today = Time.now.to_date
 
