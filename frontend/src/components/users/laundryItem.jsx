d0c9531150332564cc25f2687351f9106d9a47c6
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-21 20:04:23 +0900

    Fix: 画像の表示

--- frontend/src/components/users/laundryItem.jsx
+++ frontend/src/components/users/laundryItem.jsx
@@ -2,6 +2,7 @@ import React from "react"
 
 import bear from "../../images/laundries/bear.svg"
 import curtains from "../../images/laundries/curtains.svg"
+import defaultImage from "../../images/laundries/tops01.png"
 import washing from "../../images/laundries/washing.svg"
 import right from "../../images/laundries/right.svg"
 
@@ -14,27 +15,24 @@ import Cookies from "js-cookie";
 const laundryImage = (image) => {
     switch (image) {
         case "curtains":
-            return "curtains";
-        case "duvet-cover":
-            return "duvet";
-        case "blanket":
-            return "blanket";
-        case "pillow":
-            return "pillow";
-        case "cushion":
-            return "cushion";
-        case "sheets":
-            return "sheets";
-        case "mat":
-            return "mattress";
+            return curtains;
+        // case "duvet-cover":
+        //     return duvet;
+        // case "blanket":
+        //     return blanket;
+        // case "pillow":
+        //     return pillow;
+        // case "cushion":
+        //     return cushion;
+        // case "sheets":
+        //     return sheets;
+        // case "mat":
+        //     return mattress;
         case "bear":
-            return "bear";
-        case "default":
-            return "default";
+            return bear;
         default:
-            return "default";
+            return defaultImage;
     }
-
 };
 
 const LaundryItem = ({id, name, image, limitDays}) => {
@@ -90,7 +88,7 @@ const LaundryItem = ({id, name, image, limitDays}) => {
     return (
         <>
             <div className="flex bg-pink-200 h-full w-full justify-around items-center">
-                <img src={curtains} alt={`${name}の画像`} className="h-3/5 w-auto"/>
+                <img src={laundryImage(image)} alt={`${name}の画像`} className="h-3/5 w-auto"/>
                 <p>{name}</p>
                 <p>{howManyDays(limitDays)}</p>
                 <div className="flex h-4/5 items-center">
