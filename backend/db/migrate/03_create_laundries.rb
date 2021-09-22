aa64f07006f754a16e91b0d06d4cadd7468f0715
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-22 19:07:33 +0900

    Fix: 7日後をデフォルトにする

--- backend/db/migrate/03_create_laundries.rb
+++ backend/db/migrate/03_create_laundries.rb
@@ -6,8 +6,7 @@ class CreateLaundries < ActiveRecord::Migration[6.1]
       t.string :name, null: false, limit: 127
       t.string :description, limit: 255
       t.integer :days, comment: "次の洗濯までの期間"
-      t.date :wash_at, null: false, default: -> { '(CURRENT_DATE)' }, comment: "次回の洗濯日"
-      # t.column :wash_at, 'DATE NOT NULL DEFAULT (CURRENT_DATE)'
+      t.date :wash_at, null: false, default: -> { '(DATE_ADD(CURRENT_DATE,INTERVAL 7 DAY))' }, comment: "次回の洗濯日"
       t.text :notice, comment: "洗濯期間が過ぎたときの通知文"
       t.string :image, default: "default", null: false, limit: 127
       t.datetime :deleted_at
