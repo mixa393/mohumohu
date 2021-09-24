3f5ef90570faaadb3be851fd3a64b587f7f1edd8
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-24 21:14:07 +0900

    Fix: laundry factory data

--- backend/spec/factories/laundry.rb
+++ backend/spec/factories/laundry.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     team
     user
     name { "#{user.name}の洗濯物" }
-    wash_at { Time.now.to_date + rand(0..10) }
-    days { rand(3..10) }
+    wash_at { Time.now.to_date + rand(0..3) }
+    days { rand(1..5) }
     description { "#{name}の説明文" }
     notice { "#{name}の通知文" }
   end
