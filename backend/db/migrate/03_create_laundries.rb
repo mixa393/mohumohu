ec87710f1da71f26552cb5c79a14935baf51c912
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-24 15:46:33 +0900

    Add: is_displayed comment

--- backend/db/migrate/03_create_laundries.rb
+++ backend/db/migrate/03_create_laundries.rb
@@ -9,7 +9,7 @@ class CreateLaundries < ActiveRecord::Migration[6.1]
       t.date :wash_at, null: false, default: -> { '(CURRENT_DATE + INTERVAL 7 DAY)' }, comment: "次回の洗濯日"
       t.text :notice, comment: "洗濯期間が過ぎたときの通知文"
       t.string :image, default: "default", null: false, limit: 127
-      t.boolean :is_displayed, null: false, default: "false"
+      t.boolean :is_displayed, null: false, default: "false", comment: "その日画面に出力するか否か"
       t.datetime :deleted_at
       t.timestamps
     end
