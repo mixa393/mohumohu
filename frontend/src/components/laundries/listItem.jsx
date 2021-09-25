d634804676f27e032b277b5e8f70d086abb4a63b
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-25 13:37:16 +0900

    Fix: mapにindex追加、アイテム名調整

--- frontend/src/components/laundries/listItem.jsx
+++ frontend/src/components/laundries/listItem.jsx
@@ -8,19 +8,20 @@ const ListItem = ({id, name, image, weekly}) => {
         <tr className="h-20">
             <td className="h-full border-2 border-dotted border-gray-100 relative">
                 <div className="absolute inset-0 h-4/5 w-4/5 t-shirts m-auto flex items-center">
-                    <div className="m-auto">{name}</div>
+                    {/*後でイメージ入れる*/}
+                    <p className="m-auto break-all">{name}</p>
                 </div>
             </td>
 
-            {weekly?.map((day) => {
+            {weekly?.map((day,index) => {
                 if (day === 2) {
-                    return (<td className={`border-2 border-dotted border-gray-100 bg-pink-200`}>
+                    return (<td key={index} className={`border-2 border-dotted border-gray-100 bg-pink-200`}>
                         <img src={washingMachine} alt="洗濯する日" className="w-3/5 m-auto"/>
                     </td>)
                 } else if (day === 1) {
-                    return (<td className={`border-2 border-dotted border-gray-100 bg-pink-100`}></td>)
+                    return (<td key={index} className={`border-2 border-dotted border-gray-100 bg-pink-100`}></td>)
                 } else {
-                    return (<td className={`border-2 border-dotted border-gray-100`}></td>)
+                    return (<td key={index} className={`border-2 border-dotted border-gray-100`}></td>)
                 }
             })}
         </tr>
