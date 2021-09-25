8849880ff3e6d210cee643f538c44cdd0c79fe8f
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-25 13:09:20 +0900

    Add: weeklyの表示

--- frontend/src/components/laundries/listItem.jsx
+++ frontend/src/components/laundries/listItem.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import washingMachine from "../../images/laundries/washing-machine.svg"
 
-const ListItem = ({name,schedule}) => {
+const ListItem = ({id, name, image, weekly}) => {
 
 
     return (
         <tr className="h-32">
             <td className="h-full border-2 border-dotted border-gray-100 relative">
                 <div className="absolute inset-0 h-4/5 w-4/5 t-shirts m-auto flex items-center">
-                    <div className="m-auto">{name ?? 'Tシャツ'}</div>
+                    <div className="m-auto">{name}</div>
                 </div>
             </td>
-            {schedule?.map((day)=>{
-            return (<td className={`border-2 border-dotted border-gray-100`}>
-                <img src={washingMachine} alt=""/>
-            </td>)
-            }) ?? [<td></td>,<td></td>,<td></td>,<td></td>,<td></td>,<td></td>,<td></td>]}
+
+            {weekly?.map((day) => {
+                if (day === 2) {
+                    return (<td className={`border-2 border-dotted border-gray-100 bg-pink-200`}>
+                        <img src={washingMachine} alt="洗濯する日" className="w-3/5 m-auto"/>
+                    </td>)
+                } else if (day === 1) {
+                    return (<td className={`border-2 border-dotted border-gray-100 bg-pink-100`}></td>)
+                } else {
+                    return (<td className={`border-2 border-dotted border-gray-100`}></td>)
+                }
+            })}
         </tr>
     );
 }
