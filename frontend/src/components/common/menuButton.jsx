7d50e3575f428fd4df127807ec0e62a4cdd9e07e
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-27 14:13:26 +0900

    Fix: メニュー画像の大きさ変更

--- frontend/src/components/common/menuButton.jsx
+++ frontend/src/components/common/menuButton.jsx
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 const MenuButton = ({handleMouseDown}) => {
     return (
         <Link className="h-16 bg-pink-200 flex justify-items-center items-center" to="/laundries" onMouseDown={handleMouseDown}>
-            <img src={menu} alt="メニュー" className="h-1/2 w-auto mx-auto"/>
+            <img src={menu} alt="メニュー" className="h-2/5 w-auto mx-auto"/>
         </Link>
     );
 
