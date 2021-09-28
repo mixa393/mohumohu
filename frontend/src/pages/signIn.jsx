bfb8a6f8fb98a0cc982c64fd05eb23632c309ff4
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-28 20:55:21 +0900

    Add: サインイン画面にロゴ追加

--- frontend/src/pages/signIn.jsx
+++ frontend/src/pages/signIn.jsx
@@ -4,6 +4,7 @@ import Cookies from "js-cookie"
 
 import {AuthContext} from "../App"
 import {signIn} from "../lib/api/auth"
+import mofit from "../images/common/mofit.png"
 
 // サインイン用ページ
 const SignIn = () => {
@@ -44,16 +45,21 @@ const SignIn = () => {
 
     return (
         <>
-            <form>
-                <label htmlFor="email">メールアドレス</label>
-                <input type="email" id="email" name="email"
-                       value={email}
-                       onChange={event => setEmail(event.target.value)}/>
+            <img src={mofit} alt="ロゴ" className="bg-pink-200 w-60 h-auto rounded-xl border-8"/>
+            <form className="flex flex-col w-2/3 mx-auto max-w-lg justify-around">
+                <div className="flex flex-col">
+                    <label htmlFor="email">メールアドレス</label>
+                    <input type="email" id="email" name="email"
+                           value={email}
+                           onChange={event => setEmail(event.target.value)}/>
+                </div>
 
-                <label htmlFor="password">パスワード</label>
-                <input type="password" id="password" name="password"
-                       value={password}
-                       onChange={event => setPassword(event.target.value)}/>
+                <div className="flex flex-col">
+                    <label htmlFor="password">パスワード</label>
+                    <input type="password" id="password" name="password"
+                           value={password}
+                           onChange={event => setPassword(event.target.value)}/>
+                </div>
 
                 <input type="submit"
                        disabled={!email || !password} // 空欄があった場合はボタンを押せないように
