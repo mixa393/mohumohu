4ca86b9918df4c236852d5bb58d4a54fb6e3d3da
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-09-28 16:05:51 +0900

    Fix: loading設定

--- frontend/src/components/common/loading.jsx
+++ frontend/src/components/common/loading.jsx
@@ -1,7 +1,14 @@
-const Loading = () =>{
-    return(
-        <p>loading</p>
-    )
+import {useState} from 'react'
+import ReactLoading from 'react-loading';
+
+const Loading = (props, children) =>{
+    const [isLoading, setIsLoading] = useState(false)
+
+    if(isLoading) {
+        return <ReactLoading type="spin" color="#ffffff" height={'20%'} width={'20%'} />
+    } else {
+        return children
+    }
 }
 
 export default Loading
