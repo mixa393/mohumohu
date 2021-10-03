c79f66b8b3ef8a2acb079300eb15929b39938117
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-10-03 17:17:00 +0900

    Fix: footerにid追加

--- frontend/src/components/common/footer.jsx
+++ frontend/src/components/common/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-            <footer className="bg-pink-100">@copyright</footer>
+            <footer className="mt-auto bg-pink-100">&copy; mixa393</footer>
     );
 }
 
