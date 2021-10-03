1eb1ad38d6b6eae71a4fe646f33debe349a244d4
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-10-03 14:06:12 +0900

    Fix: teamsInfoレイアウト修正 縦の余白はflexboxで管理

--- frontend/src/pages/settings/teamsInfo.jsx
+++ frontend/src/pages/settings/teamsInfo.jsx
@@ -73,8 +73,9 @@ const TeamsInfo = () => {
         if (isDisplayedForm) {
             return (
                 <>
-                    <form onSubmit={handleUpdateTeam}>
-                        <div className="mt-10 w-3/4 mx-auto">
+                    <form onSubmit={handleUpdateTeam}
+                          className="flex flex-col justify-around h-1/2 mt-4 w-3/4 mx-auto">
+                        <div className="w-full mx-auto">
                             <label htmlFor="name" className="text-left block">チーム名</label>
                             <input type="text" id="name" name="name"
                                    className="bg-gray-100 p-2 w-full focus:outline-none focus:ring"
@@ -85,7 +86,7 @@ const TeamsInfo = () => {
 
                         </div>
 
-                        <div className="mt-10 w-3/4 mx-auto">
+                        <div className="w-full mx-auto">
                             <label htmlFor="locationId" className="text-left block">
                                 天気を表示する地域
                             </label>
@@ -127,13 +128,13 @@ const TeamsInfo = () => {
                         <input type="submit"
                                onClick={handleUpdateTeam}
                                value="変更"
-                               className="bg-blue-300 max-w-1/2 mt-14 mx-auto py-2 px-4"/>
+                               className="bg-blue-300 max-w-1/2 mx-auto py-2 px-6"/>
                     </form>
 
                     <button onClick={() => {
                         setIsDisplayedForm(false)
                     }}
-                            className="bg-gray-100 max-w-1/2 mt-4 mx-auto py-2 px-4">
+                            className="bg-gray-100 max-w-1/2 mx-auto py-2 px-6">
                         戻る
                     </button>
                 </>
@@ -141,22 +142,24 @@ const TeamsInfo = () => {
         } else {
             return (
                 <>
-                    <div className="mt-10 w-3/4 mx-auto">
-                        <h2 className="text-left">チーム名</h2>
-                        <p className="bg-gray-100 p-2">{currentTeam.name}</p>
-                    </div>
+                    <div className="flex flex-col justify-around h-2/5 mt-4 w-3/4 mx-auto">
+                        <div className="w-full mx-auto">
+                            <h2 className="text-left">チーム名</h2>
+                            <p className="bg-gray-100 p-2">{currentTeam.name}</p>
+                        </div>
 
-                    <div className="mt-10 w-3/4 mx-auto">
-                        <h2 className="text-left">天気を表示する地域</h2>
-                        <p className="bg-gray-100 p-2">{currentTeam.locationId}</p>
-                    </div>
+                        <div className="w-full mx-auto">
+                            <h2 className="text-left">天気を表示する地域</h2>
+                            <p className="bg-gray-100 p-2">{currentTeam.locationId}</p>
+                        </div>
 
-                    <button onClick={() => {
-                        setIsDisplayedForm(true)
-                    }}
-                            className="bg-blue-300 max-w-1/2 mt-20 mx-auto p-3">
-                        変更する
-                    </button>
+                        <button onClick={() => {
+                            setIsDisplayedForm(true)
+                        }}
+                                className="bg-blue-300 max-w-1/2 mx-auto p-3">
+                            変更する
+                        </button>
+                    </div>
                 </>
             )
         }
@@ -164,9 +167,9 @@ const TeamsInfo = () => {
 
 
     return (
-        <div className="h-screen flex flex-col">
+        <div className="h-screen">
             <Loading isLoading={loading}>
-                <h1 className="text-xl mt-10">チーム情報</h1>
+                <h1 className="text-xl mt-4">チーム情報</h1>
                 {contents(isDisplayedForm)}
             </Loading>
         </div>
