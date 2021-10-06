3dcff549c43cb4e95a0f37b740ba010fe8daadff
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-10-06 16:48:45 +0900

    Add: 洗濯物新規追加ボタン

--- frontend/src/pages/laundries/index.jsx
+++ frontend/src/pages/laundries/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import List from "../../components/laundries/list";
+import {Link} from "react-router-dom";
 
 
 const LaundriesIndex = () => {
@@ -7,7 +8,7 @@ const LaundriesIndex = () => {
 
     return (
         <>
-            <button>laundry新規追加</button>
+            <Link to="/laundries/add" className="bg-yellow-200 hover:bg-white p-3">新規追加</Link>
             <List/>
         </>
     );
