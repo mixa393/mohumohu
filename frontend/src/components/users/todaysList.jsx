2fb449a61010d16118d78b894677a7ef57bf573b
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-10-08 20:10:24 +0900

    Add: 洗濯物が何も無い時

--- frontend/src/components/users/todaysList.jsx
+++ frontend/src/components/users/todaysList.jsx
@@ -1,31 +1,39 @@
 import React from "react";
 import TodaysListItem from "./todaysListItem"
+import sunGray from "../../images/common/sunGray.svg"
 
-const TodaysList = ({laundries,update}) => {
+const TodaysList = ({laundries, update}) => {
     // 今日の洗濯ものがある時とない時でコンテンツを分ける
     let contents;
     if (laundries.length > 0) {
         contents = laundries.map((laundry, index) => {
             return (
-                <TodaysListItem
-                    key={index}
-                    index={index}
-                    id={laundry.id}
-                    name={laundry.name}
-                    image={laundry.image}
-                    isDisplayed={laundry.isDisplayed}
-                    limitDays={laundry.limitDays}
-                    update={update}
-                />
+                <>
+                    <h2 className="bg-yellow-100 py-3 text-yellow-600">今日の洗濯物</h2>
+                    <TodaysListItem
+                        key={index}
+                        index={index}
+                        id={laundry.id}
+                        name={laundry.name}
+                        image={laundry.image}
+                        isDisplayed={laundry.isDisplayed}
+                        limitDays={laundry.limitDays}
+                        update={update}
+                    />
+                </>
             )
         })
     } else {
-        contents = <div>何もない時</div>
+        contents = <div style={{minHeight: 'calc(100vh - 5.5rem - 18rem)'}}
+                        className="bg-gray-100 h-full flex items-center justify-center space-x-2">
+            <img src={sunGray} alt="" className="h-7 w-auto"/>
+            <p className="text-gray-400">今日の洗濯物はありません</p>
+            <img src={sunGray} alt="" className="h-7 w-auto"/>
+        </div>
     }
 
     return (
         <>
-            <div className="bg-yellow-100 py-3">今日の洗濯物リスト</div>
             {contents}
         </>
     );
