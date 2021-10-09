0f86819a5aeb3571161fbe54c66e0fbd47e4904b
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-10-09 17:45:44 +0900

    Fix: オプションでクラスを渡す

--- frontend/src/components/common/button.jsx
+++ frontend/src/components/common/button.jsx
@@ -1,10 +1,11 @@
 
-const Button = ({color,func,value}) => {
+const Button = ({color,func,value,option}) => {
+    const options = option ?? ""
     return(
         <input type="submit"
                onClick={func}
                value={value}
-               className={`bg-${color}-300 w-1/2 mx-auto py-2 rounded-xl border-b-4 border-${color}-500 hover:bg-${color}-400`}/>
+               className={`bg-${color}-300 mx-auto py-2 rounded-xl border-b-4 border-${color}-500 hover:bg-${color}-400 ${options}`}/>
     )
 }
 
