d90fa5470df6591e85235d2408e530eaddfbb1fa
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-10-11 11:04:42 +0900

    Add: remind_atの初期値

--- backend/spec/factories/user.rb
+++ backend/spec/factories/user.rb
@@ -5,5 +5,6 @@ FactoryBot.define do
     email { Faker::Internet.unique.email }
     password { Faker::Internet.password }
     password_confirmation { password }
+    remind_at { "09:00" }
   end
 end
