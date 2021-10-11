a29b8f733be064a58b6980f75d4af30bfd375f94
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-10-11 11:32:09 +0900

    Add: update_paramsに入れ忘れのプロパティ

--- backend/app/controllers/api/v1/auth/registrations_controller.rb
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -31,6 +31,6 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
   end
 
   def account_update_params
-    params.permit(:name, :email, :password, :password_confirmation)
+    params.permit(:name, :team_id, :remind_at, :email, :password, :password_confirmation)
   end
 end
