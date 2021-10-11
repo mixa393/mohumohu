d693a500fd4beb929d437f9d48b22e05a07bec27
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-10-11 11:31:09 +0900

    Add: 論理削除する時にメールアドレスも変更する

--- backend/app/controllers/api/v1/auth/registrations_controller.rb
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -6,7 +6,9 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
   # 論理削除 deleted_atカラムを現在日時に更新
   # @see: https://github.com/heartcombo/devise/wiki/How-to:-Soft-delete-a-user-when-user-deletes-account
   def destroy
-    resource.soft_delete
+    user = User.find(params[:id])
+    soft_delete(user)
+
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
     set_flash_message :notice, :destroyed
     yield resource if block_given?
@@ -14,6 +16,15 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
   end
 
   private
+
+  # 論理削除メソッド
+  # メールアドレスとdeleted_atを変更
+  def soft_delete(user)
+    deleted_email = 'deleted_' + I18n.l(Time.current, format: :delete_flag) + user.email
+    user.assign_attributes(email: deleted_email, deleted_at: Time.current)
+    user.skip_reconfirmation!
+    user.save!
+  end
 
   def sign_up_params
     params.permit(:name, :team_id, :remind_at, :email, :password, :password_confirmation)
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -16,11 +16,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }, length: { maximum: 127 }
 
-  # instead of deleting, indicate the user requested a delete & timestamp it
-  def soft_delete
-    update_attribute(:deleted_at, Time.current)
-  end
-
   # ensure user account is active
   def active_for_authentication?
     super && !deleted_at
