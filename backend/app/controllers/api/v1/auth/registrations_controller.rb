8a68701326cbb79bc3d53299d9a615a0aa43dfc4
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-10-11 12:09:13 +0900

    Fix: ページ移動はしない

--- backend/app/controllers/api/v1/auth/registrations_controller.rb
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -12,7 +12,6 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
     # set_flash_message :notice, :destroyed
     yield resource if block_given?
-    # respond_with_navigational(resource) { redirect_to after_sign_out_path_for(resource_name) }
   end
 
   private
